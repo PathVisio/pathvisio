915393306ddcc53d404e1e913c39350494941da4
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2006-03-12 10:53:31 +0000

    Moved roelands work to trunk

---
+++ trunk/Ensembl work/Create_Ensembl_database.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS `Ensembl`;
+CREATE DATABASE `Ensembl`;
+USE `Ensembl`;
+SOURCE `C:\homo_sapiens_37.sql`;
+SOURCE `C:\read_fundamental.txt`;
+SOURCE `C:\read_external.txt`;
+SOURCE `C:\read_features.txt`;
+SOURCE `C:\read_ID.txt`;
+SOURCE `C:\read_misc.txt`;
+

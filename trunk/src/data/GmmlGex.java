bd89044cdf2930ed8dc017cce2057bc905e77829
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2006-05-23 10:12:41 +0000

    - fixed bug in drawing of new objects (wiki nr 10) - added markDirty() in Brace when moving/resizing

--- trunk/src/data/GmmlGex.java
+++ trunk/src/data/GmmlGex.java
@@ -3,9 +3,11 @@ package data;
 import graphics.GmmlGeneProduct;
 import graphics.GmmlGpColor;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
--- trunk/src/util/SwtUtils.java
+++ trunk/src/util/SwtUtils.java
@@ -1,6 +1,8 @@
 package util;
 
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
@@ -14,5 +16,15 @@ public class SwtUtils {
 			cOld = null;
 		}
 		return new Color(display, rgbNew);
+	}
+	
+	public static Font changeFont(Font fOld, FontData fd, Display display)
+	{
+		if(fOld != null && !fOld.isDisposed())
+		{
+			fOld.dispose();
+			fOld = null;
+		}
+		return new Font(display, fd);
 	}
 }

bb9f470cff7c20995f1d94a70b80c280da8c4df4
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2006-07-31 06:56:20 +0000

    some conflicts after last commit...resolved

--- trunk/src/preferences/GmmlPreferenceManager.java
+++ trunk/src/preferences/GmmlPreferenceManager.java
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 package preferences;
 
 import org.eclipse.jface.preference.ColorFieldEditor;
@@ -82,71 +81,3 @@ public class GmmlPreferenceManager extends PreferenceManager {
 		}
 	}
 }
-=======
-package preferences;
-
-import org.eclipse.jface.preference.ColorFieldEditor;
-import org.eclipse.jface.preference.DirectoryFieldEditor;
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.PreferenceManager;
-import org.eclipse.jface.preference.PreferenceNode;
-
-public class GmmlPreferenceManager extends PreferenceManager {
-	
-	public GmmlPreferenceManager() {
-		super();
-
-	    PreferenceNode colors = new PreferenceNode("colors", new ColorsPage());
-	    PreferenceNode directories = new PreferenceNode("directories", new DirectoriesPage());
-	    
-	    addToRoot(colors);
-	    addToRoot(directories);
-	}
-	
-	
-	private class DirectoriesPage extends FieldEditorPreferencePage {
-		public DirectoriesPage() {
-			super("Directories", GRID);
-			noDefaultAndApplyButton();
-		}
-		
-		protected void createFieldEditors() {
-			DirectoryFieldEditor d1 = new DirectoryFieldEditor("directories.gmmlFiles",
-					"Gmml pathways:", getFieldEditorParent());
-			addField(d1);
-			
-			DirectoryFieldEditor d2 = new DirectoryFieldEditor("directories.gdbFiles",
-					"Gene databases:", getFieldEditorParent());
-			addField(d2);
-			
-			DirectoryFieldEditor d3 = new DirectoryFieldEditor("directories.exprFiles",
-					"Expression datasets:", getFieldEditorParent());
-			addField(d3);
-		}
-	}
-	
-	private class ColorsPage extends FieldEditorPreferencePage {
-		public ColorsPage() {
-			super("Colors", GRID);
-		}
-		
-		protected void createFieldEditors() {
-			ColorFieldEditor f1 = new ColorFieldEditor("colors.no_criteria_met", 
-					"Default color for 'no criteria met':", getFieldEditorParent());
-			addField(f1);
-			ColorFieldEditor f2 = new ColorFieldEditor("colors.no_gene_found", 
-					"Default color for 'gene not found':", getFieldEditorParent());
-			addField(f2);
-			ColorFieldEditor f3 = new ColorFieldEditor("colors.no_data_found", 
-					"Default color for 'no data found':", getFieldEditorParent());
-			addField(f3);
-			ColorFieldEditor f4 = new ColorFieldEditor("colors.selectColor", 
-					"Line color for selected objects:", getFieldEditorParent());
-			addField(f4);
-			ColorFieldEditor f5 = new ColorFieldEditor("colors.highlightColor", 
-					"Line color for highlighted objects:", getFieldEditorParent());
-			addField(f5);
-		}
-	}
-}
->>>>>>> .r341

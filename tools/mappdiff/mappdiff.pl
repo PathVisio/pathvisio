0c99962e349c7f544767c120aa9fc3f93154feb5
martijn <amarillion@users.noreply.github.com>
2006-08-07 13:26:26 +0000

    mappdiff tool completed.

--- tools/mappdiff/mappdiff.pl
+++ tools/mappdiff/mappdiff.pl
@@ -14,9 +14,17 @@ use File::Basename;
 
 mappdiff.pl - compare two GenMAPP mapp files.
 
+This tool should be flexible enough to handle all kinds 
+of differences:
+insertion of objects
+deletion of objects
+change of one or a few fields of an object
+
+mappdiff.pl returns the number of differences found as exit code
+
 =head1 usage
 
- mappdiff.pl file1 file2
+ mappdiff.pl file1 file2 
 
 =cut
 
@@ -25,7 +33,6 @@ if (!(@ARGV == 2 && -r $ARGV[0] && -r $ARGV[1]))
 	die "Incorrect number of command line arguments, ".
 		"or one of the files is not readable\n"; 
 }
-
 
 # connect to gdb
 my $g_cDsn = 1;
@@ -50,12 +57,12 @@ sub get_db_handle
 my $fnMapp1 = $ARGV[0];
 my $fnMapp2 = $ARGV[1];
 
-my $dbh1 = get_db_handle ($fnMapp1);
-my $dbh2 = get_db_handle ($fnMapp2);
 
-
+# read all object data from mapp 1
 my @data1;
+my $info1;
 {
+	my $dbh1 = get_db_handle ($fnMapp1);
 	my $sth = $dbh1->prepare("SELECT * FROM OBJECTS");
 	$sth->execute() or die "SQL error: $DBI::errstr";
 	while (my $row = $sth->fetchrow_hashref)
@@ -63,10 +70,16 @@ my @data1;
 		push @data1, $row;
 	}
 	@data1 = sort by_object_row @data1;
+	$sth = $dbh1->prepare("SELECT * FROM INFO");
+	$sth->execute() or die "SQL error: $DBI::errstr";
+	$info1 = $sth->fetchrow_hashref;	
 }
 	
+#read all object data from mapp 2
 my @data2;
+my $info2;
 {
+	my $dbh2 = get_db_handle ($fnMapp2);
 	my $sth = $dbh2->prepare("SELECT * FROM OBJECTS");
 	$sth->execute() or die "SQL error: $DBI::errstr";
 	while (my $row = $sth->fetchrow_hashref)
@@ -74,8 +87,14 @@ my @data2;
 		push @data2, $row;
 	}
 	@data2 = sort by_object_row @data2;
+	$sth = $dbh2->prepare("SELECT * FROM INFO");
+	$sth->execute() or die "SQL error: $DBI::errstr";
+	$info2 = $sth->fetchrow_hashref;	
 }
 
+# calculate a match score between two object rows
+# if this is below a certain value, assumed to be completely different
+# if this is above a certain value, assumed that just a few fields changed
 sub object_rows_hitscore
 {
 	my $row1 = shift;
@@ -83,77 +102,105 @@ sub object_rows_hitscore
 	my $score = 0;
 	for (qw(ID SystemCode Type CenterX CenterY SecondX SecondY Width Height Rotation Color Label Head Remarks Image Links Notes))
 	{
+		no warnings;
 		if ($$row1{$_} eq $$row2{$_}) { $score++; }
 	}
 	return $score;
 }
 
+# Compares two rows column by column
+# prints all columns that differ
 sub object_row_diff
 {
 	my $row1 = shift;
 	my $row2 = shift;
+	my $output;
 	for (qw(ID SystemCode Type CenterX CenterY SecondX SecondY Width Height Rotation Color Label Head Remarks Image Links Notes))
 	{
 		no warnings;
 		unless ($$row1{$_} eq $$row2{$_}) 
 		{
-			print "Col $_ : '", $$row1{$_}, "' <-> '", $$row2{$_}, "'\n";
+			$output .= "Column $_ : '" . $$row1{$_} . "' <-> '" . $$row2{$_} . "'\n";
 		}
+	}
+	if ($output)
+	{
+		print "In row Type='", $$row1{Type}, "' ID='", $$row1{ID}, "' CenterX='", 
+			$$row1{CenterX}, "' CenterY='", $$row1{CenterY}, "' :\n", $output;
 	}
 }
 
-sub by_object_row
+# Compares two rows column by column
+# prints all columns that differ
+sub info_row_diff
 {
+	my $row1 = shift;
+	my $row2 = shift;
+	my $output;
+	for (qw(Title MAPP GeneDB GeneDBVersion Version Author Maint Email Copyright Modify Remarks BoardWidth BoardHeight WindowWidth WindowHeight Notes))
+	{
+		no warnings;
+		unless ($$row1{$_} eq $$row2{$_}) 
+		{
+			$output .= "Column $_ : '" . $$row1{$_} . "' <-> '" . $$row2{$_} . "'\n";
+		}
+	}
+	if ($output)
+	{
+		print "In Info row: \n$output";
+	}
+}
+
+# sorting routine
+# Sorts on all columns, the ones most unlikely to change first
+sub by_object_row ($$)
+{
+	my $a = shift;
+	my $b = shift;
 	for my $col (qw(Type ID CenterX CenterY SystemCode SecondX SecondY Width Height Rotation Color Label Head Remarks Image Links Notes))
 	{
 		no warnings;
-		print exists $$a{$col} ? "" : $col;
 		my $result = $$a{$col} cmp $$b{$col};
 		if ($result != 0) { return $result; }
 	}	
 }
 
-print scalar @data1, "\t", scalar @data2, "\n";
+my $diffCount = 0;
 
-for (1..4)
-{	
-	object_row_diff (shift @data1, shift @data2);
+info_row_diff ($info1, $info2);
+
+while (@data1 > 0 || @data2 > 0)
+{
+	my $row1 = shift @data1;
+	my $row2 = shift @data2;
+	my $score = object_rows_hitscore ($row1, $row2);
+	if ($score != 17)
+	{
+		$diffCount ++;
+	}
+	if ($score >= 14)
+	{
+		object_row_diff ($row1, $row2);
+	}
+	else
+	{
+		my $cmp = by_object_row ($row1, $row2);
+		if ($cmp < 0)
+		{
+			print "Unique in 1: ";
+			print "row Type='", $$row1{Type}, "' ID='", $$row1{ID}, "' CenterX='", 
+				$$row1{CenterX}, "' CenterY='", $$row1{CenterY}, "'\n";
+			unshift @data2, $row2;
+		}
+		else
+		{
+			print "Unique in 2: ";
+			print "row Type='", $$row2{Type}, "' ID='", $$row2{ID}, "' CenterX='", 
+				$$row2{CenterX}, "' CenterY='", $$row2{CenterY}, "'\n";
+			unshift @data1, $row1;
+		}
+	}
 }
 
-#~ while (1)
-#~ {
-
-	
-	
-	#~ my $max_score = 0;
-	#~ my $max_row = undef;
-	#~ my $max_index = undef;
-	#~ my $type = $$row{Type};
-
-	#~ for (my $i = 0; $i < scalar @{$data2{$type}}; ++$i)
-	#~ {
-		#~ my $row = ${$data2{$type}[$i];
-		#~ my $score = object_rows_hitscore ($row, $row2)		
-		#~ if ($score > $max_score)
-		#~ {
-			#~ $max_score = $score;
-			#~ $max_row = $row2;
-			#~ $max_index = $i;
-		#~ }		
-	#~ }
-		
-	#~ if ($max_score > 14)
-	#~ {
-		#~ #not so perfect.
-		#~ #which cols are differing?
-		#~ object_row_diff ($max_row, $row);
-		#~ splice (@{$data2{$type}}, $max_index, 1);
-	#~ }
-	#~ else
-	#~ {
-		#~ #nothing matches
-		#~ print "Only in 1:\n", Dumper ($row);
-	#~ }
-#~ }
-
-
+print "Found $diffCount differences\n";
+exit ($diffCount);

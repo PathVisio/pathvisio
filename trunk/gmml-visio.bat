60cec2c28776a8355a98bae33c7d35ea643d7be2
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2006-09-09 14:16:18 +0000

    updated gmml-visio.bat created java webstart (incomplete) created web page

--- trunk/gmml-visio.bat
+++ trunk/gmml-visio.bat
@@ -1,3 +1,3 @@
-SET MY_CLASS_PATH="lib/org.eclipse.core.runtime_3.1.2.jar";"lib/org.eclipse.jface_3.1.1.jar";"lib/swt.jar";"lib/jdom.jar";build;"lib/hsqldb.jar"
+SET MY_CLASS_PATH="lib/JRI.jar";"lib/org.eclipse.core.commands_3.2.0.I20060605-1400.jar";"lib/org.eclipse.equinox.common_3.2.0.v20060603.jar";"lib/org.eclipse.jface_3.2.0.I20060605-1400.jar";"lib/org.eclipse.swt_3.2.0.v3232o.jar";"lib/jdom.jar";build;"lib/hsqldb.jar";"lib/org.eclipse.swt.win32.win32.x86_3.2.0.v3232m.jar"
 
 java -cp %MY_CLASS_PATH% gmmlVision.GmmlVisionMain

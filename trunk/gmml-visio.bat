23c555ae1dcadbca53394ae994ee6d952d4b9e0c
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2006-09-25 07:23:55 +0000

    fixed bug #2: misalignment of arcs fixed bug #19: xsd is now in resources.jar removed resources.jar etc, these are build intermediates

--- trunk/gmml-visio.bat
+++ trunk/gmml-visio.bat
@@ -1,3 +1,3 @@
-SET MY_CLASS_PATH="lib/JRI.jar";"lib/org.eclipse.core.commands_3.2.0.I20060605-1400.jar";"lib/org.eclipse.equinox.common_3.2.0.v20060603.jar";"lib/org.eclipse.jface_3.2.0.I20060605-1400.jar";"lib/org.eclipse.swt_3.2.0.v3232o.jar";"lib/jdom.jar";build;"lib/hsqldb.jar";"lib/org.eclipse.swt.win32.win32.x86_3.2.0.v3232m.jar"
+SET MY_CLASS_PATH="lib/JRI.jar";"lib/org.eclipse.equinox.common.jar";"lib/org.eclipse.equinox.supplement.jar";"lib/org.eclipse.jface.jar";"lib/swt-win32.jar";"lib/org.eclipse.core.commands.jar";"lib/jdom.jar";build;"lib/hsqldb.jar";"lib/swt-win32-lib.jar";"lib/resources.jar"
 
 java -cp %MY_CLASS_PATH% gmmlVision.GmmlVisionMain

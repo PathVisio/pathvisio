faadf7bff7fb941d2b68742be48ec8d320fa0e28
Thomas Kelder <thomaskelder@users.noreply.github.com>
2006-10-09 11:33:57 +0000

    - added StopWatch class for easy time measurement

---
+++ src/debug/StopWatch.java
@@ -0,0 +1,29 @@
+package debug;
+
+import gmmlVision.GmmlVision;
+
+public class StopWatch {
+	boolean running;
+	long start;
+	long last;
+	
+	public void start() {
+		start = System.currentTimeMillis();
+		running = true;
+	}
+	
+	public long stop() {
+		last = System.currentTimeMillis() - start;
+		running = false;
+		return last;
+	}
+		
+	public long look() {
+		if(running) return System.currentTimeMillis() - start;
+		return last;
+	}
+	
+	public void stopToLog(String msg) {
+		GmmlVision.log.trace(msg + "; t = " + stop());
+	}
+}

f24b633d87cfb515ebb40c7e8a306ed7f7981d60
martijn <amarillion@users.noreply.github.com>
2006-11-24 14:18:24 +0000

    fixed bug in linux start script, removed attic

--- gmml-visio.sh
+++ gmml-visio.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/atlas:/usr/lib/mozilla-firefox
-export LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/atlas:/usr/lib/mozilla
+export MOZILLA_FIVE_HOME=/usr/lib/mozilla
 java -cp lib/JRI.jar:lib/org.eclipse.equinox.common.jar:lib/org.eclipse.equinox.supplement.jar:lib/org.eclipse.jface.jar:lib/swt-linux.jar:lib/org.eclipse.core.commands.jar:lib/jdom.jar:build:lib/hsqldb.jar:lib/swt-linux-lib.jar:lib/resources.jar -Djava.library.path="/usr/lib/mozilla:./lib/swt-linux-lib" gmmlVision.GmmlVisionMain

8a179f908dca6e2677bc29496648c5159ff5d8f6
martijn <amarillion@users.noreply.github.com>
2007-01-16 12:47:56 +0000

    compilation error in v1: removed extraneous legend.refresh() in v1

--- pathvisio.sh
+++ pathvisio.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/atlas:/usr/lib/mozilla
-export MOZILLA_FIVE_HOME=/usr/lib/mozilla
-java -cp lib/JRI.jar:lib/org.eclipse.equinox.common.jar:lib/org.eclipse.equinox.supplement.jar:lib/org.eclipse.jface.jar:lib/swt-linux.jar:lib/org.eclipse.core.commands.jar:lib/jdom.jar:build/v1:lib/hsqldb.jar:lib/swt-linux-lib.jar:lib/resources.jar -Djava.library.path="/usr/lib/mozilla:./lib/swt-linux-lib" gmmlVision.GmmlVisionMain
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/atlas:/usr/lib/firefox
+export MOZILLA_FIVE_HOME=/usr/lib/firefox
+java -cp lib/JRI.jar:lib/org.eclipse.equinox.common.jar:lib/org.eclipse.equinox.supplement.jar:lib/org.eclipse.jface.jar:lib/swt-linux.jar:lib/org.eclipse.core.commands.jar:lib/jdom.jar:build/v1:lib/hsqldb.jar:lib/swt-linux-lib.jar:lib/resources.jar -Djava.library.path="/usr/lib/firefox:./lib/swt-linux-lib" gmmlVision.GmmlVisionMain

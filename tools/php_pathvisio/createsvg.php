8089e8ebbbe5a5f77e2fa53127dd05db4310cc6d
andra <andrawaag@users.noreply.github.com>
2007-02-13 11:03:49 +0000

    eerste commit om wachtwoord te testen

--- tools/php_pathvisio/createsvg.php
+++ tools/php_pathvisio/createsvg.php
@@ -39,11 +39,12 @@ stroke:#000000;stroke-width:1\"/>");
    
 }
 
+$deel_factor = 10;
 $doc_complete = new DOMDocument();
 $doc_complete->load('Hs_Fatty_Acid_Beta_Oxidation_1_BiGCaT.gpml');
 $entry1 = $doc_complete->getElementsByTagName("Graphics");
-$boardwidth = intval($entry1->item(0)->getAttribute("BoardWidth"))/15;
-$boardheight = intval($entry1->item(0)->getAttribute("BoardHeight"))/15;
+$boardwidth = intval($entry1->item(0)->getAttribute("BoardWidth"))/$deel_factor;
+$boardheight = intval($entry1->item(0)->getAttribute("BoardHeight"))/$deel_factor*4;
 $svg = fopen('test.svg', 'w+');
 fwrite($svg, "<?xml version=\"1.0\" standalone=\"no\"?>\n
 <!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20001102//EN\"\n
@@ -51,18 +52,33 @@ fwrite($svg, "<?xml version=\"1.0\" standalone=\"no\"?>\n
 <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"
 version=\"1.0\" width=\"$boardheight\" height=\"$boardheight\">\n\n");
 
+//print arrows
+$entries = $doc_complete->getElementsByTagName("Line");
+foreach ($entries as $entry){
+	$start_x = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(0)->getAttribute("x"))/$deel_factor;
+	$start_y = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(0)->getAttribute("y"))/$deel_factor;
+	$stop_x = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(1)->getAttribute("x"))/$deel_factor;
+	$stop_y = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(1)->getAttribute("y"))/$deel_factor;
+	arrow($svg, $start_x, $start_y, $stop_x, $stop_y, 3, 3, 1);
+}
+
+
 //GeneProducts
 $entries = $doc_complete->getElementsByTagName("GeneProduct");
 foreach ($entries as $entry){
-	$y_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterY"))/15;
-	$x_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterX"))/15;
-	$width = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("Width"))/15;
-	$height = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("Height"))/15;
 
-
+	$y_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterY"))/$deel_factor;
+	$x_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterX"))/$deel_factor;
+	$width = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("Width"))/$deel_factor;
+	$height = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("Height"))/$deel_factor;
+        $x_cor = $x_cor-($width/2);
+	$y_cor = $y_cor-($height/2);
 	fwrite($svg, "<rect x=\"$x_cor\" y=\"$y_cor\" style=\"fill:rgb(255,255,255);stroke-width:1;
 	stroke:rgb(0,0,0)\" width=\"$width\" height=\"$height\"/>\n\n");
-        $font = "Arial";
+
+	$y_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterY"))/$deel_factor;
+	$x_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterX"))/$deel_factor;
+        $font = "Century.ttf";
 	$fontsize = 12;
 	$text = $entry->getAttribute("GeneID");
         $angle = 0;
@@ -70,16 +86,17 @@ foreach ($entries as $entry){
 	$wordlength = $wordlengtharray[4]-$wordlengtharray[6];
 	$wordheigtharray = imagettfbbox($fontsize, 0, $font, $text);
 	$wordheigth = $wordheightarray[7]-$wordheightarray[5];
+	$x_cor = $x_cor - ($wordlength/2);
+	$y_cor = $y_cor + 5;
 	fwrite($svg, "<text x=\"$x_cor\" y=\"$y_cor\" style=\"font-family:$font;font-size:$fontsize\"> $text
 		</text>\n");
-
 }
 
 //Print labels
 $entries = $doc_complete->getElementsByTagName("Label"); 
 foreach ($entries as $entry){
-	$y_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterY"))/15;
-	$x_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterX"))/15;
+	$y_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterY"))/$deel_factor;
+	$x_cor = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("CenterX"))/$deel_factor;
 	//$wordlength = strlen($entry->getAttribute("TextLabel"));
 	$fontsize = intval($entry->getElementsByTagName("Graphics")->item(0)->getAttribute("FontSize"));
 	$text = $entry->getAttribute("TextLabel");
@@ -93,21 +110,15 @@ foreach ($entries as $entry){
 	$wordlength = $wordlengtharray[4]-$wordlengtharray[6];
 	$wordheigtharray = imagettfbbox($fontsize, 0, $font, $text);
 	$wordheigth = $wordheightarray[7]-$wordheightarray[5];
+	$x_cor = $x_cor - ($wordlength/2);
+	$y_cor = $y_cor + ($wordheigth/2);
 	fwrite($svg, "<text x=\"$x_cor\" y=\"$y_cor\" style=\"font-family:$font;font-size:$fontsize\"> $text
 		</text>\n");
 		//imagettftext($im,$fontsize, $angle, $x_cor-($wordlength/2), $y_cor-($wordheigth/2), $text_color, $font, $text);
 	//	imagestring($im, 5, $x_cor, $y_cor, $text, $text_color);
 }
 
-//print arrows
-$entries = $doc_complete->getElementsByTagName("Line");
-foreach ($entries as $entry){
-	$start_x = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(0)->getAttribute("x"))/15;
-	$start_y = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(0)->getAttribute("y"))/15;
-	$stop_x = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(1)->getAttribute("x"))/15;
-	$stop_y = intval($entry->getElementsByTagName("Graphics")->item(0)->getElementsByTagName("Point")->item(1)->getAttribute("y"))/15;
-	arrow($svg, $start_x, $start_y, $stop_x, $stop_y, 3, 3, 1);
-}
+
 
 fwrite($svg, "</svg>");
 fclose($svg);

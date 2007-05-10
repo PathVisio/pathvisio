ca877f8863bb2437956cb7c047d5c205b7ffd347
martijn <amarillion@users.noreply.github.com>
2007-05-10 22:50:39 +0000

    Updated Gpml defaults, and the script to generate them
    Fixed issue #133

--- tools/get_defaults/get_defaults.pl
+++ tools/get_defaults/get_defaults.pl
@@ -10,19 +10,49 @@ use strict;
 use warnings;
 use XML::Twig;
 
-my @list;
+# %group is used to store attribGroup information
+my %group;
 
+#define a set of handlers
 my $twig = XML::Twig->new( twig_handlers => 
 	{ 
-		"xsd:attribute" => \&attrib,
+		# handle all attribute tags below an element tag
+		"xsd:element//xsd:attribute" => \&attrib,
+		
+		# handle all attribute tags below an attributeGroup tag
+		"xsd:attributeGroup/xsd:attribute" => \&groupAttrib,
+		
+		# handle all attributeGroups below complexType
+		"xsd:complexType/xsd:attributeGroup" => \&groupRef,
 	}
 );
 
+# run all the handlers on the XSD
 $twig->parsefile ("../../GPML.xsd");
 $twig->purge;
 
+# handler for references to attribute Groups
+sub groupRef
+{
+	my $t = shift;
+	my $s = shift;
+	my $ref = $s->att("ref"); 
 
-sub attrib 
+	my $containingtype = get_containing_type($s);
+	
+	# look up the stuff previously stored in %group.
+	# for each attribute stored, print a java statement
+	for my $attr (@{$group{$ref}})
+	{
+		printStatement($containingtype, 
+			$$attr{name}, $$attr{type}, 
+			$$attr{default}, $$attr{use});
+	}
+}
+
+# handler for declaration of attribGroups
+# this will store info on an attribGroup in a %group
+sub groupAttrib
 {
 	my $t = shift;
 	my $s = shift;
@@ -38,30 +68,86 @@ sub attrib
 	{
 		$use="optional";
 	}
-	my @containers;
+	
 	my $default = $s->att("default");
 	
-	my $pp = $s;
+	my $pp = $s->parent("xsd:attributeGroup");
+	my $groupName = "gpml:" . $pp->{'att'}->{'name'}; 
+	
+	push @{$group{$groupName}}, 
+		{
+			name => $name,
+			type => $type,
+			use => $use,
+			default => $default
+		};	
+}
+
+# print a java statement for a particular attribute
+sub printStatement
+{
+	my $containingtype = shift;
+	my $name = shift;
+	my $type = shift;
+	my $default = shift;
+	my $use = shift;
+
+	print "result.put(\"$containingtype\@$name\", new AttributeInfo (\"$type\", ", 
+		(defined($default) ? "\"$default\"" : "null"), 
+		", \"$use\"));\n";
+	
+}
+
+# find the containing elements for a certain attribute,
+# e.g. it will try to find "Label.Graphics" for the attribute "FontWeight"
+sub get_containing_type
+{
+	my $pp = shift;
+	my @containers;
 	do 
 	{
+		# look for any ancestor element of the type "xsd:element"
 		$pp = $pp->parent ("xsd:element");
 		if (defined $pp)
 		{
 			push @containers, $pp->att("name");
 		}
 	}
-	while (defined $pp);
-	
+	while (defined $pp);	
 	my $containingtype = join ".", reverse @containers;
+}
+
+# handle an ordinary attribute definition
+sub attrib 
+{
+	my $t = shift;
+	my $s = shift;
 	
-	#~ if ($containingtype eq "Graphics")
-	#~ {
-		#~ $containingtype = $s->parent->parent->parent->parent->parent->att("name") . ".Graphics";
-	#~ }
+	# if a parent element is xsd:group, then we're in an element
+	# group definition such as BiopaxGroup. Skip.
 	
-	print "result.put(\"$containingtype\@$name\", new AttributeInfo (\"$type\", ", 
-		(defined($default) ? "\"$default\"" : "null"), 
-		", \"$use\"));\n";
+	my $name = $s->att("name"); 
+	my $type = $s->att("type");
+	if (!defined $type)
+	{
+		$type = $s->first_child("xsd:simpleType")->first_child("xsd:restriction")->att("base");
+	}
+	my $use = $s->att("use"); 
+	if (!defined $use)
+	{
+		$use="optional";
+	}
+	my $default = $s->att("default");
 	
-	#~ print "$name\t$type\t$use\t$default\t$containingtype\n";
+	my $containingtype;
+	if (defined $s->parent("xsd:group"))
+	{
+		$containingtype = get_containing_type($s);
+	}
+	else
+	{
+		$containingtype = get_containing_type($s);
+	}
+	
+	printStatement($containingtype, $name, $type, $default, $use);
 }

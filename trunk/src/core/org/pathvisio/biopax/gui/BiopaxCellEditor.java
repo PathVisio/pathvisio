9a4331cfcf48802964f1e6b46ed6e5aab6388b1b
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-05-30 19:34:46 +0000

    - worked on biopax editor - updated webstart and run scripts for new swt library files - added licence info to new source files

--- trunk/src/core/org/pathvisio/biopax/gui/BiopaxCellEditor.java
+++ trunk/src/core/org/pathvisio/biopax/gui/BiopaxCellEditor.java
@@ -1,41 +1,43 @@
+// PathVisio,
+// a tool for data visualization and analysis using Biological Pathways
+// Copyright 2006-2007 BiGCaT Bioinformatics
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+//  
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
 package org.pathvisio.biopax.gui;
 
-import java.util.List;
-
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.jface.viewers.DialogCellEditor;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.jdom.Document;
-import org.pathvisio.gui.ButtonCellEditor;
+import org.eclipse.swt.widgets.Control;
+import org.pathvisio.biopax.BiopaxManager;
 import org.pathvisio.gui.Engine;
 
-public class BiopaxCellEditor extends ButtonCellEditor {
-	Document biopax;
-	
+public class BiopaxCellEditor extends DialogCellEditor {	
 	public BiopaxCellEditor(Composite parent, String label) {
-		super(parent, label);
+		super(parent);
 	}
 	
-	protected void setSelectionListeners(Button b) {
-		b.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				BiopaxDialog d = new BiopaxDialog(e.display.getActiveShell());
-				d.setPathway(Engine.getPathway());
-				d.open();
-			}
-		});
+	protected Button createButton(Composite parent) {
+		Button b = super.createButton(parent);
+		b.setText("...");
+		return b;
 	}
 
-	protected Object doGetValue() {
-		return biopax;
-	}
-
-	protected void doSetValue(Object obj) {
-		if(!(obj instanceof List) && obj != null) {
-			throw new IllegalArgumentException("Can't set object of class " + obj.getClass());
-		} else {
-			biopax = (Document)obj;
-		}
+	protected Object openDialogBox(Control cellEditorWindow) {
+		BiopaxManager bpm = new BiopaxManager(Engine.getPathway().getBiopax().getBiopax());
+		BiopaxRefDialog d = new BiopaxRefDialog(cellEditorWindow.getShell(), bpm, (String)getValue());
+		d.open();
+		return d.getRef();
 	}
 }

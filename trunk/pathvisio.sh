f72941941c28ba7ded99e8d948195a77a31629eb
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-06-29 15:16:57 +0000

    Merged pathvisio start scripts into one with options

--- trunk/pathvisio.sh
+++ trunk/pathvisio.sh
@@ -1,8 +1,50 @@
-#!/bin/sh
+#!/bin/bash
+
+# RUN_MODE can be "DIRECT" or "WEBSTART"
+RUN_MODE=DIRECT
+# MAIN_CLASS contains the main class to run when RUN_MODE is DIRECT
+MAIN_CLASS=org.pathvisio.gui.swt.GuiMain
+# BASE_URL contains the webstart url (without pathvisio_v1.jnlp or pathvisio_v2.jnlp)
+BASE_URL=
+# Set USE_EXPERIMENTAL to 1 if you want to run with Data visualizatoin and R mode
+USE_EXPERIMENTAL=0
+
+while getopts ":gerd" options; do
+	case $options in
+		g )
+			RUN_MODE=DIRECT
+			MAIN_CLASS=org.pathvisio.gui.swing.GuiMain
+			BASE_URL=
+			;;
+		e )
+			USE_EXPERIMENTAL=1
+			;;
+		r )
+			RUN_MODE=WEBSTART
+			MAIN_CLASS=
+			BASE_URL=http://ftp2.bigcat.unimaas.nl/~martijn.vaniersel/pathvisio/daily/webstart
+			;;
+		d )
+			RUN_MODE=WEBSTART
+			MAIN_CLASS=
+			BASE_URL=http://blog.bigcat.unimaas.nl/~gmmlvisio
+			;;
+		\? )
+			echo "Usage: `basename $0` [-g|-r|-d] [-e] [-?]"
+			echo "  -g : Use swing instead of swt"
+			echo "  -r : Use webstart, latest stable release"
+			echo "  -d : Use webstart, daily build"
+			echo "  -e : Turn on experimental features (Data visualization, statistics)"
+			echo "  -? : show this help message"
+			exit;
+			;;
+	esac
+done
+
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/atlas:/usr/lib/firefox
 export MOZILLA_FIVE_HOME=/usr/lib/firefox
 
-MYCLASSPATH=lib/JRI.jar:\
+MYCLASSPATHBASE=lib/JRI.jar:\
 lib/BrowserLauncher.jar:\
 lib/org.eclipse.equinox.common.jar:\
 lib/org.eclipse.equinox.supplement.jar:\
@@ -10,7 +52,6 @@ lib/org.eclipse.jface.jar:\
 lib/swt-linux-lib/swt.jar:\
 lib/org.eclipse.core.commands.jar:\
 lib/jdom.jar:\
-build/v1:\
 lib/derby.jar:\
 lib/swt-linux-lib.jar:\
 lib/resources.jar:\
@@ -31,4 +72,19 @@ lib/batik/batik-svggen.jar:\
 lib/batik/batik-bridge.jar:\
 lib/batik/batik-gvt.jar
 
-java -cp $MYCLASSPATH -Djava.library.path="/usr/lib/firefox:./lib/swt-linux-lib" org.pathvisio.gui.GuiMain 
+MYCLASSPATH1=$MYCLASSPATHBASE:build/v1
+MYCLASSPATH2=$MYCLASSPATHBASE:build/v2
+
+if [ $RUN_MODE = "DIRECT" ]; then
+	if [ $USE_EXPERIMENTAL = "0" ]; then
+		java -cp $MYCLASSPATH1 -Djava.library.path="/usr/lib/firefox:./lib/swt-linux-lib" $MAIN_CLASS
+	elif [ $USE_EXPERIMENTAL = "1" ]; then
+		java -cp $MYCLASSPATH2 -Djava.library.path="/usr/lib/firefox:./lib/swt-linux-lib" $MAIN_CLASS -ur
+	fi	
+elif [ $RUN_MODE = "WEBSTART" ]; then
+	if [ $USE_EXPERIMENTAL = "0" ]; then
+		javaws "$BASE_URL/pathvisio_v1.jnlp"
+	elif [ $USE_EXPERIMENTAL = "1" ]; then
+		javaws "$BASE_URL/pathvisio_v2.jnlp" -ur
+	fi
+fi
--- trunk/pv2_daily.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export MOZILLA_FIVE_HOME=/usr/lib/firefox
-export LD_LIBRARY_PATH=/usr/lib/R/lib:/usr/lib/firefox:$LD_LIBRARY_PATH
-export R_HOME=/usr/lib/R
-javaws "http://ftp2.bigcat.unimaas.nl/~martijn.vaniersel/pathvisio/daily/webstart/pathvisio_v2.jnlp"

8a3bce9dccd248611286f5f90aa9af0898e9c5de
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-06-29 12:02:26 +0000

    deleted some redundant code

--- trunk/src/g2d/org/pathvisio/preferences/PreferenceCollection.java
+++
@@ -1,9 +0,0 @@
-package org.pathvisio.preferences;
-
-import java.io.IOException;
-
-public interface PreferenceCollection {	
-	public Preference byName(String name);
-	
-	public void save() throws IOException;
-}

10cab1bf31064cdac9dd3758a56217a63be410d6
martijn <amarillion@users.noreply.github.com>
2007-08-07 11:51:13 +0000

    * moved daily build script * removed double license headers *again*

--- tools/dailybuild/add_license_header.sh
+++ tools/dailybuild/add_license_header.sh
@@ -2,9 +2,11 @@
 
 # extract the license header from a file that has it:
 head -n 16 src/core/org/pathvisio/Engine.java > license.txt
-# create a list of files missing the license header
-find . -name "*.java" ! -name "Revision.java" -exec \
-  grep -L "Apache License, Version 2.0" '{}' \; > license_check.txt
+# create a list of files missing the license header (exclude Revision.java)
+find . -name "*.java" ! -name "Revision.java" \
+  | xargs -d '\n' grep -L "Apache License, Version 2.0" \ 
+  | xargs -d '\n' grep -L "Copyright (C) 2005 Mammoth Software LLC" \
+  > license_check.txt
 # add contents of license.txt at the beginning of those files.
 for i in $( cat license_check.txt ); do
   cp $i $i.bak;

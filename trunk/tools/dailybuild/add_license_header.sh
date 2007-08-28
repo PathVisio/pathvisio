785142dfbb67b69b837a20c1319b48c510cf860e
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-08-28 12:04:00 +0000

    added licence headers

--- trunk/tools/dailybuild/add_license_header.sh
+++ trunk/tools/dailybuild/add_license_header.sh
@@ -3,9 +3,9 @@
 # extract the license header from a file that has it:
 head -n 16 ../../src/core/org/pathvisio/Engine.java > license.txt
 # create a list of files missing the license header (exclude Revision.java)
-find ../../ -name "*.java" ! -name "Revision.java" \
-  | xargs -d '\n' grep -L "Apache License, Version 2.0" \ 
-  | xargs -d '\n' grep -L "Copyright (C) 2005 Mammoth Software LLC" \
+find ../.. -name "*.java" ! -name "Revision.java" \
+ | xargs -d '\n' grep -L "Apache License, Version 2.0" \
+ | xargs -d '\n' grep -L "Copyright (C) 2005 Mammoth Software LLC" \
   > license_check.txt
 # add contents of license.txt at the beginning of those files.
 for i in $( cat license_check.txt ); do

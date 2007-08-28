1038d5901d6db76a1443963120694d35ea598608
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-08-28 11:47:14 +0000

    added licence headers

--- trunk/tools/dailybuild/add_license_header.sh
+++ trunk/tools/dailybuild/add_license_header.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
 # extract the license header from a file that has it:
-head -n 16 src/core/org/pathvisio/Engine.java > license.txt
+head -n 16 ../../src/core/org/pathvisio/Engine.java > license.txt
 # create a list of files missing the license header (exclude Revision.java)
-find . -name "*.java" ! -name "Revision.java" \
+find ../../ -name "*.java" ! -name "Revision.java" \
   | xargs -d '\n' grep -L "Apache License, Version 2.0" \ 
   | xargs -d '\n' grep -L "Copyright (C) 2005 Mammoth Software LLC" \
   > license_check.txt

06abb63bf371e9f5f66b86ad9c3f4937831cb323
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-09-12 20:19:28 +0000

    fixed create new pathway page to use applet

--- trunk/wikipathways/wpi/extensions/CreatePathwayPage/CreatePathwayPage_body.php
+++ trunk/wikipathways/wpi/extensions/CreatePathwayPage/CreatePathwayPage_body.php
@@ -10,29 +10,55 @@ class CreatePathwayPage extends SpecialPage
 
         function execute( $par ) {
                 global $wgRequest, $wgOut, $wpiScriptURL;
-                
                 $this->setHeaders();
-				
-				$html = tag('p', 'To create a new pathway, specify the pathway name and species 
-									and then click "create pathway" to open the pathway editor.<br>
-									You can then draw your pathway and close the editor to save it to WikiPathway.org');
-				$html .= "	<input type='hidden' name='action' value='new'>
-							<td>Pathway name:
-							<td><input type='text' name='pwName'>
-							<tr><td>Species:<td>
-							<select name='pwSpecies'>";
-				$species = Pathway::getAvailableSpecies();
-				foreach($species as $sp) {
-					$html .= "<option value=$sp" . (!$selected ? ' selected' : '') . ">$sp";
-					$selected = true;
-				}
-				$html .= '</select>';
-				$html = tag('table', $html);
-				$html .= tag('input', "", array('type'=>'submit', 'value'=>'Create pathway'));
-				$html = tag('form', $html, array('action'=>$wpiScriptURL, 'method'=>'get'));
-				$html .= "<HR><A href='http://blog.bigcat.unimaas.nl/~gmmlvisio/pathvisio_v1.jnlp'>Create new local pathway</A>";
-				$wgOut->addHTML($html);
-        }
+
+		if($_GET['action'] == 'new') { //Submit button pressed
+			$this->startEditor($_GET['pwName'], $_GET['pwSpecies']);
+		} else {
+			$this->showForm();
+		}        }
+
+	function startEditor($pwName, $pwSpecies) {
+		global $wgRequest, $wgOut, $wpiScriptURL;
+
+		$backlink = '<a href="javascript:history.back(-1)">Back</a>';
+		if(!$pwName) {
+			$wgOut->addHTML("<B>Please specify a name for the pathway<BR>$backlink</B>");
+			return;
+		}
+		if(!$pwSpecies) {
+			$wgOut->addHTML("<B>Please specify a species for the pathway<BR>$backlink</B>");
+			return;
+		}
+
+		//TODO
+		$pathway = new Pathway($pwName, $pwSpecies, false); //new pathway, but no caching
+		$wgOut->addHTML("<div id='applet'></div>");
+		$pwTitle = $pathway->getTitleObject()->getText();
+		$wgOut->addWikiText("{{#editApplet:direct|applet|true|$pwTitle}}");
+	}
+
+	function showForm() {
+		global $wgRequest, $wgOut, $wpiScriptURL;
+		$html = tag('p', 'To create a new pathway on WikiPathways, specify the pathway name and species 
+				and then click "create pathway" to start the pathway editor.<br>'
+				);
+		$html .= "	<input type='hidden' name='action' value='new'>
+				<td>Pathway name:
+				<td><input type='text' name='pwName'>
+				<tr><td>Species:<td>
+				<select name='pwSpecies'>";
+		$species = Pathway::getAvailableSpecies();
+		foreach($species as $sp) {
+			$html .= "<option value=$sp" . (!$selected ? ' selected' : '') . ">$sp";
+			$selected = true;
+		}
+		$html .= '</select>';
+		$html = tag('table', $html);
+		$html .= tag('input', "", array('type'=>'submit', 'value'=>'Create pathway'));
+		$html = tag('form', $html, array('action'=>"http://" . $_SERVER['HTTP_HOST'] . '/index.php/Special:CreatePathwayPage', 'method'=>'get'));
+		$wgOut->addHTML($html);
+	}
 
         function loadMessages() {
                 static $messagesLoaded = false;

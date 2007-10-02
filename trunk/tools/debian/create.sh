3d0c48cf6a915ebc60698bec30ee220f2207b158
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-10-02 12:50:50 +0000

    Improved debian package: now doesn't include all the .svn directories

--- trunk/tools/debian/create.sh
+++ trunk/tools/debian/create.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-dpkg-deb --build nbx-pathvisio-1.0
+TEMPDIR=`mktemp -d` || exit 1
+find nbx-pathvisio -! -iwholename "*.svn*" -exec cp --parent '{}' $TEMPDIR \;
+dpkg-deb --build $TEMPDIR/nbx-pathvisio .

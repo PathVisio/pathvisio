e97b556dc0dd150c85c84fbf6dda84fd4249bba1
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-10-08 13:11:17 +0000

    Improved deb package (fixed lintian warnings)

--- trunk/tools/debian/create.sh
+++ trunk/tools/debian/create.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+# create a temporary dir
 TEMPDIR=`mktemp -d` || exit 1
-find nbx-pathvisio -! -iwholename "*.svn*" -exec cp --parent '{}' $TEMPDIR \;
-dpkg-deb --build $TEMPDIR/nbx-pathvisio .
+
+# copy all files to temporary dir, excluding .svn dirs
+find nbx-pathvisio -type f -! -iwholename "*.svn*" -exec cp --parent '{}' $TEMPDIR \;
+
+# build the package (fakeroot needed to simulate root:root ownership)
+fakeroot dpkg-deb --build $TEMPDIR/nbx-pathvisio .
+
+echo Checking package with lintian
+
+# do sanity checks
+lintian nbx-pathvisio*.deb

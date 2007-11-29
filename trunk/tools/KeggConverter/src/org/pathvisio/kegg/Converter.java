b5b233911545e1e80c8acbcff668045e1c874fe6
A.vanAdrichem <A.vanAdrichem@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-11-29 15:14:51 +0000

    Work on KEGG converter

--- trunk/tools/KeggConverter/src/org/pathvisio/kegg/Converter.java
+++ trunk/tools/KeggConverter/src/org/pathvisio/kegg/Converter.java
@@ -2,12 +2,21 @@ package org.pathvisio.kegg;
 
 import java.io.File;
 import java.io.IOException;
+import java.rmi.RemoteException;
 import java.util.List;
+
+import javax.xml.rpc.ServiceException;
+
+import keggapi.KEGGLocator;
+import keggapi.KEGGPortType;
 
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
+import org.pathvisio.model.ObjectType;
+import org.pathvisio.model.Pathway;
+import org.pathvisio.model.PathwayElement;
 
 public class Converter {
 
@@ -16,32 +25,109 @@ public class Converter {
 	 */
 	public static void main(String[] args) {
 		String filename = "examples/map00031.xml";
-		
+		String specie = "hsa";
+
 		SAXBuilder builder  = new SAXBuilder();
 		try {
 			Document doc = builder.build(new File(filename));
-			System.out.println(doc);
-			Element rootelement =doc.getRootElement();
-			System.out.println(rootelement);
-			List<Element> kindjes=rootelement.getChildren();
-			System.out.println(kindjes);
-			for(Element child : kindjes) {
-				System.out.println(child);
-				Element substrate = child.getChild("substrate");
-				System.out.println("Name: " + child.getAttributeValue("name"));
-				System.out.println("\t" + substrate);
-			}
 
-			
-			
-			
-			
-		} catch (JDOMException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+			Element rootelement =doc.getRootElement();
+
+			List<Element> grandChildren=rootelement.getChildren();
+
+			Pathway pathway = new Pathway();
+
+			for(Element child : grandChildren) {
+
+				String type = child.getAttributeValue("type");
+				if(type != null) {
+					/** types: map, enzyme, compound **/
+
+					if(type.equals("enzyme")) 
+					{
+						Element graphics = child.getChild("graphics");
+
+						String enzymeCode = child.getAttributeValue("name");
+						String[] ncbi = getNcbiByEnzyme(enzymeCode, specie); //Gencodes --> ID
+
+						for(int i=1; i<=ncbi.length; i++ )
+						{
+							String textlabelGPML; // naam van gen i uit online NCBI database
+							String colorGPML = child.getAttributeValue("fgcolor");
+							String centerXGPML = child.getAttributeValue("x");
+							String centerYGPML = child.getAttributeValue("y");
+							String widthGPML = child.getAttributeValue("width");
+							String heightGPML = child.getAttributeValue("height");
+
+							Double height = Double.parseDouble(heightGPML);
+							Double centerY = Double.parseDouble(centerYGPML) - i*height;
+								
+							PathwayElement element = new PathwayElement(ObjectType.DATANODE);
+							String id = element.getGraphId();
+							element.setTextLabel(textlabelGPML);
+							element.setDataNodeType("Geneproduct");
+//							element.setColor(colorGPML);
+							element.setMCenterX(Double.parseDouble(centerXGPML));
+							element.setMCenterY(centerY);
+							element.setMWidth(Double.parseDouble(widthGPML));
+							element.setMHeight(height);
+							element.setDataSource("Entrez Gene"); 
+							element.setGeneID(ncbi[i]);
+
+							if(element != null) {
+								pathway.add(element);
+							}
+						}
+					}
+					else if(type.equals("compound"))
+					{
+						String textlabelGPML = ; // naam van metabolite uit online KEGG database
+
+						Element graphics = child.getChild("graphics");
+						String colorGPML = child.getAttributeValue("fgcolor");
+						String centerXGPML = child.getAttributeValue("x");
+						String centerYGPML = child.getAttributeValue("y");
+						String widthGPML = child.getAttributeValue("width");
+						String heightGPML = child.getAttributeValue("height");
+
+						PathwayElement element = new PathwayElement(ObjectType.DATANODE);
+						String id = element.getGraphId();
+						element.setTextLabel(textlabelGPML);
+						element.setDataNodeType("Metabolite");
+//						element.setColor(colorGPML);
+						element.setMCenterX(Double.parseDouble(centerXGPML));
+						element.setMCenterY(Double.parseDouble(centerYGPML));
+						element.setMWidth(Double.parseDouble(widthGPML));
+						element.setMHeight(Double.parseDouble(heightGPML));
+						
+						pathway.add(element);
+					}					
+					else if(type.equals("map"))
+					{
+						String textlabelGPML = null; 
+						String typeGPML = null;
+						
+						PathwayElement element = new PathwayElement(ObjectType.LABEL);
+					}
+				}
+			}	
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	
 	}
 
+	public static String[] getNcbiByEnzyme(String ec, String species) throws ServiceException, RemoteException 
+	{
+		//Setup a connection to KEGG
+		KEGGLocator  locator = new KEGGLocator();
+		KEGGPortType serv;
+		serv = locator.getKEGGPort();
+
+		//Fetch the gene names
+		String[] genes = serv.get_genes_by_enzyme(ec, species);
+
+		//KEGG code --> NCBI code
+
+		return new String[] { "test" };
+	}
 }

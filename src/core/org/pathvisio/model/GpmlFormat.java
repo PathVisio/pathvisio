74bfe6fdcb4dc41004e58818d1eedf8da52362e0
martijn <amarillion@users.noreply.github.com>
2008-01-24 14:15:41 +0000

    small fix to prevent null values in converter

--- src/core/org/pathvisio/model/GpmlFormat.java
+++ src/core/org/pathvisio/model/GpmlFormat.java
@@ -722,7 +722,8 @@ public class GpmlFormat implements PathwayImporter, PathwayExporter
 			setAttribute ("DataNode", "Type", e, o.getDataNodeType());
 			setAttribute ("DataNode", "BackpageHead", e, o.getBackpageHead());
 			Element xref = e.getChild("Xref", e.getNamespace());
-			setAttribute ("DataNode.Xref", "Database", xref, o.getDataSource() == null ? "" : o.getDataSource().getFullName());
+			String database = o.getDataSource() == null ? "" : o.getDataSource().getFullName();
+			setAttribute ("DataNode.Xref", "Database", xref, database == null ? "" : database);
 			setAttribute ("DataNode.Xref", "ID", xref, o.getGeneID());
 		}
 	}

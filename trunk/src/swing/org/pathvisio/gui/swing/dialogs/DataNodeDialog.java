0aa2b8f5ac08ae48bf61d49c8e7d3a81d216de1a
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-02-11 12:14:47 +0000

    changed GUI for datanode identifier entry - split up in search/manual - improved keyboard shortcuts

--- trunk/src/swing/org/pathvisio/gui/swing/dialogs/DataNodeDialog.java
+++ trunk/src/swing/org/pathvisio/gui/swing/dialogs/DataNodeDialog.java
@@ -1,38 +1,42 @@
-// PathVisio,
-// a tool for data visualization and analysis using Biological Pathways
-// Copyright 2006-2007 BiGCaT Bioinformatics
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-//  
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
-// limitations under the License.
-//
+//PathVisio,
+//a tool for data visualization and analysis using Biological Pathways
+//Copyright 2006-2007 BiGCaT Bioinformatics
+
+//Licensed under the Apache License, Version 2.0 (the "License"); 
+//you may not use this file except in compliance with the License. 
+//You may obtain a copy of the License at 
+
+//http://www.apache.org/licenses/LICENSE-2.0 
+
+//Unless required by applicable law or agreed to in writing, software 
+//distributed under the License is distributed on an "AS IS" BASIS, 
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+//See the License for the specific language governing permissions and 
+//limitations under the License.
+
 package org.pathvisio.gui.swing.dialogs;
 
 import java.awt.Component;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingUtilities;
+import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -53,7 +57,7 @@ import org.pathvisio.util.RunnableWithProgress;
 
 public class DataNodeDialog extends PathwayElementDialog {
 	private static final long serialVersionUID = 1L;
-	
+
 	public DataNodeDialog(PathwayElement e, boolean readonly, Frame frame, Component locationComp) {
 		super(e, readonly, frame, "DataNode properties", locationComp);
 		getRootPane().setDefaultButton(null);
@@ -63,7 +67,7 @@ public class DataNodeDialog extends PathwayElementDialog {
 	CompleterQueryTextField symText;
 	CompleterQueryTextField idText;
 	JComboBox dbCombo;
-			
+
 	public void refresh() {
 		super.refresh();
 		symText.setText(getInput().getTextLabel());
@@ -75,7 +79,7 @@ public class DataNodeDialog extends PathwayElementDialog {
 		}
 		pack();
 	}
-	
+
 	private void applyAutoFill(Xref ref) {
 		Gdb gdb = GdbManager.getCurrentGdb();
 		String sym = gdb.getGeneSymbol(ref);
@@ -83,7 +87,7 @@ public class DataNodeDialog extends PathwayElementDialog {
 		idText.setText(ref.getId());
 		dbCombo.setSelectedItem(ref.getDataSource());
 	}
-	
+
 	/**
 	 * Search for symbols or ids in the synonym databases that match
 	 * the given text
@@ -91,14 +95,14 @@ public class DataNodeDialog extends PathwayElementDialog {
 	private void search(final String text) {
 		if(text == null || "".equals(text)) {
 			JOptionPane.showMessageDialog(this, "No search term specified, " +
-					"please type something in the 'Text label' field");
+			"please type something in the 'Text label' field");
 			return;
 		}
-		
+
 		final SwingProgressKeeper progress = new SwingProgressKeeper(SwingProgressKeeper.PROGRESS_UNKNOWN);
 		ProgressDialog dialog = new ProgressDialog(null, "Searching", progress, true, true);
 		dialog.setLocationRelativeTo(this);
-		
+
 		final RunnableWithProgress<List<XrefWithSymbol>> task = new RunnableWithProgress<List<XrefWithSymbol>>() {
 			public List<XrefWithSymbol> excecuteCode() {
 				return doSearch(text, progress);
@@ -114,7 +118,7 @@ public class DataNodeDialog extends PathwayElementDialog {
 		};
 		sw.execute();
 		dialog.setVisible(true);
-		
+
 		List<XrefWithSymbol> results = null;
 		try {
 			results = sw.get();
@@ -124,7 +128,7 @@ public class DataNodeDialog extends PathwayElementDialog {
 			JOptionPane.showMessageDialog(this, "Error while searching: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 			Logger.log.error("Error while searching", e);
 		}
-		
+
 		//Show results to user
 		if(results.size() > 0) {
 			DatabaseSearchDialog resultDialog = new DatabaseSearchDialog("Results", results);
@@ -137,12 +141,12 @@ public class DataNodeDialog extends PathwayElementDialog {
 			JOptionPane.showMessageDialog(this, "No results for '" + text + "'");
 		}
 	}
-	
+
 	private List<XrefWithSymbol> doSearch(String text, ProgressKeeper progress) {
 		Gdb gdb = GdbManager.getCurrentGdb();
-		
+
 		List<XrefWithSymbol> result = new ArrayList<XrefWithSymbol>();
-		
+
 		//Search for ids
 		List<Xref> ids = gdb.getIdSuggestions(text, 1000);
 		for(Xref x : ids) {
@@ -152,7 +156,7 @@ public class DataNodeDialog extends PathwayElementDialog {
 		if(progress != null && progress.isCancelled()) {
 			return result;
 		}
-		
+
 		//Search for symbols
 		List<String> symbols = gdb.getSymbolSuggestions(text, 1000);
 		for(String s : symbols) {
@@ -168,37 +172,80 @@ public class DataNodeDialog extends PathwayElementDialog {
 		progress.finished();
 		return result;
 	}
-	
+
 	protected void addCustomTabs(JTabbedPane parent) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
-		
-		JLabel symLabel = new JLabel("Text label");
-		JLabel idLabel = new JLabel("Identifier");
-		JLabel dbLabel = new JLabel("Database");
-		
+
+		JPanel searchPanel = new JPanel();
+		JPanel fieldPanel = new JPanel();
+		searchPanel.setBorder(BorderFactory.createTitledBorder("Search"));
+		fieldPanel.setBorder(BorderFactory.createTitledBorder("Manual entry"));
+		GridBagConstraints panelConstraints = new GridBagConstraints();
+		panelConstraints.fill = GridBagConstraints.HORIZONTAL;
+		panelConstraints.gridx = 0;
+		panelConstraints.weightx = 1;
+		panelConstraints.insets = new Insets(10, 10, 5, 5);
+		panelConstraints.gridy = GridBagConstraints.RELATIVE;
+
+		panel.add(searchPanel, panelConstraints);
+		panelConstraints.fill = GridBagConstraints.BOTH;
+		panelConstraints.weighty = 1;
+		panel.add(fieldPanel, panelConstraints);
+
+		//Search panel elements
+		searchPanel.setLayout(new GridBagLayout());
+
+		final JTextField searchText = new JTextField();
 		final JButton searchButton = new JButton("Search");
+
+		//Key listener to search when user presses Enter
+		searchText.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					searchButton.requestFocus();
+					search(searchText.getText());
+				}
+			}
+		});
+
 		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				search(symText.getText());			
+				search(searchText.getText());			
 			}
 		});
 		searchButton.setToolTipText("Search the synonym database for references, based on the text label");
-		
+
+		GridBagConstraints searchConstraints = new GridBagConstraints();
+		searchConstraints.gridx = GridBagConstraints.RELATIVE;
+		searchConstraints.fill = GridBagConstraints.HORIZONTAL;
+		searchConstraints.weightx = 1;
+		searchPanel.add(searchText, searchConstraints);
+
+		searchConstraints.weightx = 0;
+		searchPanel.add(searchButton, searchConstraints);
+
+		//Manual entry panel elements
+		fieldPanel.setLayout(new GridBagLayout());
+
+		JLabel symLabel = new JLabel("Text label");
+		JLabel idLabel = new JLabel("Identifier");
+		JLabel dbLabel = new JLabel("Database");
+
 		symText = new CompleterQueryTextField(new OptionProvider() {
 			public Object[] provideOptions(String text) {
 				if(text == null) return new Object[0];
-				
+
 				Gdb gdb = GdbManager.getCurrentGdb();
 				List<String> symbols = gdb.getSymbolSuggestions(text, 100);
 				return symbols.toArray();
 			}
 		}, true);
-		
+
 		idText = new CompleterQueryTextField(new OptionProvider() {
 			public Object[] provideOptions(String text) {
 				if(text == null) return new Object[0];
-				
+
 				Gdb gdb = GdbManager.getCurrentGdb();
 				List<Xref> refs = gdb.getIdSuggestions(text, 100);
 				//Only take identifiers
@@ -213,26 +260,22 @@ public class DataNodeDialog extends PathwayElementDialog {
 		idText.setCorrectCase(false);
 
 		dbCombo = new JComboBox(DataSource.getDataSources().toArray());
-		
+
 		GridBagConstraints c = new GridBagConstraints();
 		c.ipadx = c.ipady = 5;
 		c.anchor = GridBagConstraints.FIRST_LINE_START;
 		c.weightx = 0;
 		c.gridx = 0;
 		c.gridy = GridBagConstraints.RELATIVE;
-		panel.add(symLabel, c);
-		panel.add(idLabel, c);
-		panel.add(dbLabel, c);
+		fieldPanel.add(symLabel, c);
+		fieldPanel.add(idLabel, c);
+		fieldPanel.add(dbLabel, c);
 		c.gridx = 1;
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 1;
-		panel.add(symText, c);
-		panel.add(idText, c);
-		panel.add(dbCombo, c);
-		c.gridx = 2;
-		c.fill = GridBagConstraints.NONE;
-		c.weightx = 0;
-		panel.add(searchButton, c);
+		fieldPanel.add(symText, c);
+		fieldPanel.add(idText, c);
+		fieldPanel.add(dbCombo, c);
 
 		symText.getDocument().addDocumentListener(new DocumentListener() {
 			public void changedUpdate(DocumentEvent e) { setText();	}
@@ -242,7 +285,7 @@ public class DataNodeDialog extends PathwayElementDialog {
 				getInput().setTextLabel(symText.getText());
 			}
 		});
-		
+
 		idText.getDocument().addDocumentListener(new DocumentListener() {
 			public void changedUpdate(DocumentEvent e) { setText();	}
 			public void insertUpdate(DocumentEvent e) {	setText(); }
@@ -251,29 +294,29 @@ public class DataNodeDialog extends PathwayElementDialog {
 				getInput().setGeneID(idText.getText());
 			}
 		});
-		
+
 		dbCombo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				DataSource item = (DataSource)dbCombo.getSelectedItem();
 				getInput().setDataSource(item);
 			}
 		});
-		
+
 		symText.setEnabled(!readonly);
 		idText.setEnabled(!readonly);
 		dbCombo.setEnabled(!readonly);
-		
+
 		parent.add("Annotation", panel);
 		parent.setSelectedComponent(panel);
 	}
-	
+
 	protected class XrefWithSymbol extends Xref {
 		String symbol;
 		public XrefWithSymbol(Xref xref, String symbol) {
 			super(xref.getId(), xref.getDataSource());
 			this.symbol = symbol;
 		}
-		
+
 		public String getSymbol() {
 			return symbol;
 		}

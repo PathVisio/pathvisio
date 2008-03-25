dc87d9082f172bb6d83dc27f28b6eb6c087d8351
martijn <amarillion@users.noreply.github.com>
2008-03-25 13:52:37 +0000

    Begin project group 2008

---
+++ tools/project2008/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}

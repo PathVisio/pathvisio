1b9f99b1a1e7bb0ea27d4498374934da564ef2a7
Laurens <Laurens@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-03-25 21:37:52 +0000

    Now the program has some additional method's. The program

--- trunk/tools/project2008/LinkChecker.java
+++ trunk/tools/project2008/LinkChecker.java
@@ -2,6 +2,10 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.pathvisio.data.DataDerby;
+import org.pathvisio.data.DataException;
+import org.pathvisio.data.SimpleGdb;
 import org.pathvisio.model.ConverterException;
 import org.pathvisio.model.ObjectType;
 import org.pathvisio.model.Pathway;
@@ -15,9 +19,20 @@ public class LinkChecker {
 	/**
 	 * @param args
 	 * @throws ConverterException 
+	 * @throws DataException 
 	 */
-	public static void main(String[] args) throws ConverterException {
+	public static void main(String[] args) throws ConverterException, DataException {
+		// TODO: load the database and the filenames in a nice way..
+		
+		
+		
+		// load the database of the rat species
+		SimpleGdb database = new SimpleGdb("C:\\muis data\\Rn_39_34i\\Rn_39_34i.pgdb", new DataDerby(), 0);
+		
+		// create a list containing Strings that represent the filenames of a directory
 		List<String> filenames = makeFilenameList();
+		
+		// for each filename create a list containing the Xref's
 		for (String filename:filenames)
 		{
 			// load the file
@@ -28,12 +43,56 @@ public class LinkChecker {
 			boolean validate = true;
 			pway.readFromXml(file, validate);
 		
-			List<Xref> xRefList = makeXrefList(pway);
-			System.out.println("size of the xRefList: "+xRefList.size());
+			// make a list containing the Xref's 
+			List<Xref> xrefList = makeXrefList(pway);
+			
+			// as a debug tool, show how much Xref's are found in the list
+			System.out.println("size of the XrefList: "+xrefList.size());
+			
+			
+			// give the precentage of Xrefs the database contains
+			String percentage = calculatePercentage(xrefList, database);
+			System.out.println("percentage found in DB: "+percentage);
+			
 			}
 		}
 	
+	public static String calculatePercentage(List<Xref> xrefList, SimpleGdb database){
+		// count how much of the Xref's exist in the database
+		
+		// initialize two counters. One for false outcomes, and one for true outcomes
+		int countTrue = 0;
+		int countFalse = 0;
+		for (Xref reference:xrefList)
+		{
+			if (database.xrefExists(reference) == true)
+			{
+			countTrue++;
+				}
+			else
+			{
+			countFalse++;
+				}
+			}
+		
+		// calculated the total count. This has to be equal to xrefList.size. It's still here for debug purpose
+		int countTotal = countTrue + countFalse;
+		
+		// calculate the precentage of found references
+		double percentagedouble = 100*countTrue/countTotal;
+		
+		// create a string with the outcome
+		String percentage = countTrue+" of "+countTotal+" found in DB; ("+percentagedouble+"%)";
+		return percentage;
+		
+	}
+	
+	
 	public static List<String> makeFilenameList(){
+		// make a list containing strings which represent all the files in a directory. This has to be made beautifull,
+		// 'cuz this way it looks kind of sloppy. ==> Ruben :)
+		
+		
 		List<String> filenames = new ArrayList();
 		String filename = "C:\\muis data\\Rn_Apoptosis.gpml";
 		filenames.add(filename);

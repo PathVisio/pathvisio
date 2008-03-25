8e9b660ff5a71c348b60bdcae67b81789c57e34f
Laurens <Laurens@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-03-25 15:34:08 +0000

    First version of the Link Checker by Laurens and Ruben

---
+++ trunk/tools/project2008/LinkChecker.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.util.List;
+import org.pathvisio.model.ConverterException;
+import org.pathvisio.model.Pathway;
+import org.pathvisio.model.PathwayElement;
+import org.pathvisio.model.Xref;
+
+
+public class LinkChecker {
+
+
+	/**
+	 * @param args
+	 * @throws ConverterException 
+	 */
+	public static void main(String[] args) throws ConverterException {
+		// TODO Auto-generated method stub
+		String filename = ("C:\\Documents and Settings\\s040772\\PathVisio-Data\\pathways\\rat\\Rn_Apoptosis.gpml");
+		File f = new File(filename);
+		Pathway p = new Pathway();
+		p.readFromXml(f, true);
+		List<PathwayElement> pelts = p.getDataObjects();
+		for (PathwayElement v:pelts){
+			Xref reference;
+			reference = v.getXref();
+			String name = reference.getName();
+			System.out.println(name);
+			}
+		}
+	}

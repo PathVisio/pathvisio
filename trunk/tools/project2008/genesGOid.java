82bc248daca1f68f1228f20168f02392b7f1dad0
s051453 <s051453@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-04-01 16:31:29 +0000

    For each geneID the GOIds are listed

--- trunk/tools/project2008/genesGOid.java
+++ trunk/tools/project2008/genesGOid.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -35,33 +37,46 @@ public class genesGOid {
 	
 	
 	List<String> ensemblGeneIds = new ArrayList<String>();
-	List<String> gOIds = new ArrayList<String>();
 	
-	//i begint bij 1 en niet bij 0 omdat het eerste getal in de vector niet moet worden meegeteld.
+	//i begint bij 1 en niet bij 0 omdat de string telkens wordt vergeleken met de string ervoor.
 	for (int i = 1;i<(arrayGOgenes.size());i++){
-		if (!arrayGOgenes.get(i)[0].equals(arrayGOgenes.get(i-1)[0])){
-			ensemblGeneIds.add(arrayGOgenes.get(i)[0]);
-			}
+		String currentGene = arrayGOgenes.get(i)[0];
+		String previousGene = arrayGOgenes.get(i-1)[0];
+		if (!currentGene.equals(previousGene)){
+			ensemblGeneIds.add(currentGene);}
 		}
 	
-	//int k=1;
-	//for (int i = 1;i<(arrayGOgenes.size()-1);i++){
-	//	if (arrayGOgenes.get(i)[0].equals(ensemblGeneIds.get(k))){
+	Map<String,List<String>> goByGene = new HashMap<String,List<String>>();
+	List<String> gOIds = new ArrayList<String>();
+	
+	for (int i = 1;i<(arrayGOgenes.size());i++){
+		if (arrayGOgenes.get(i).length > 2){
+			String currentGene = arrayGOgenes.get(i)[0];
+			String currentGOId = arrayGOgenes.get(i)[2];
+			String previousGene = arrayGOgenes.get(i-1)[0];
 			
-		//}
-			
-	
-	//ensemblGeneIds.set(0,"test");
-	System.out.println("arrayGOgenes2"+arrayGOgenes.get(2)[0]);
-	System.out.println("arrayGOgenes3"+arrayGOgenes.get(3)[0]);
-	System.out.println("arrayGOgenes4"+arrayGOgenes.get(4)[0]);
-	
-	
-	
-	System.out.println("ensemblGeneIds2"+ensemblGeneIds.get(2));
-	System.out.println("ensemblGeneIds3"+ensemblGeneIds.get(3));
-	System.out.println("ensemblGeneIds4"+ensemblGeneIds.get(4));
+			if (!currentGene.equals(previousGene)){
+				goByGene.put(previousGene,gOIds);
+				gOIds = new ArrayList<String>();
+				gOIds.add(currentGOId);}
+			else {gOIds.add(currentGOId);}
+		}
 	}
 	
-}
+	for (String key : goByGene.keySet())
+	{
+		System.out.println (key);
+		
+		List<String> values = goByGene.get(key);
+		
+		for (String value : values)
+		{
+			System.out.println ("  " + value);
+		}
+		}
+	}
+	
+	}
+
+
 	

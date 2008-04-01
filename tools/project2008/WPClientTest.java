0c8662d61651eddfa40cb93bd3350fce539be244
Laurens <Laurens@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-04-01 12:50:49 +0000

    Now in English, with comments

--- tools/project2008/WPClientTest.java
+++ tools/project2008/WPClientTest.java
@@ -9,27 +9,32 @@ public class WPClientTest
 {
 	public static void main(String[] args) throws XmlRpcException, IOException
 	{
+		// make a new WikiPathwaysClient
 		WikiPathwaysClient wp = new WikiPathwaysClient();
 		
+		// get the pathwaylist; all the known pathways are
+		// stored in a list
 		List<String> pathwayNames = wp.getPathwayList();
 		
+		
+		// path to store the pathway cache
+		String path = "C:\\WPClient\\";
+		
+		// a for loop that downloads all individual pathways
 		for (int i = 0; i < pathwayNames.size(); ++i)
 		{
-			// path om files in op te slaan
-			String path = "C:\\WPClient\\";
-			// soort extracten
-			String pathwaynaam = pathwayNames.get(i);
-			String[] temp = pathwaynaam.split(":");
-			String soort = temp[0];
-			
-			// naam extracten
-			String naamPathway = temp[1];
+
+			// get the species and pathwayname
+			String pathwayName= pathwayNames.get(i);
+			String[] temporary = pathwayName.split(":");
+			String species = temporary[0];
+			String namePathway = temporary[1];
 			
 			//	mapje aanmaken als deze nog niet bestaat;
-			new File(path + soort + "\\").mkdir();
+			new File(path + species + "\\").mkdir();
 			
 			wp.downloadPathway(pathwayNames.get(i), 
-				new File (path + soort + "\\" + naamPathway + ".gpml"));
+				new File (path + species + "\\" + namePathway + ".gpml"));
 			System.out.println("Downloaded: " + pathwayNames.get(i));
 		}
 	}

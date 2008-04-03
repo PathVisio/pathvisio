e637d7a9ccfae0df2b18a99ef6ab828928a5fcbb
rdeneer <rdeneer@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-04-03 10:54:15 +0000

    Added roots.

--- trunk/tools/project2008/GoReader.java
+++ trunk/tools/project2008/GoReader.java
@@ -7,7 +7,18 @@ import java.util.List;
 public class GoReader {
 	
 	public static void main(String[] args) {
-		readGoDatabase(args[0]);
+		List<GoTerm> terms = new ArrayList<GoTerm>();
+		List<GoTerm> roots = new ArrayList<GoTerm>();
+		terms=readGoDatabase(args[0]);
+		roots=getRoots(terms);
+		getChildren(terms,"GO:0052182");
+		//for (GoTerm root : roots){
+		//	System.out.println(root.getId());
+		//	System.out.println(root.getName());
+		//	System.out.println(root.getNamespace());
+		//	System.out.println(root.getParents());
+		//	System.out.println();
+		//}
 	}
 	
 	public static List<GoTerm> readGoDatabase(String path){
@@ -38,16 +49,33 @@ public class GoReader {
 		    fr.close();
 		    }		
 		    catch(Exception e) {
-		      System.out.println("Exception: " + e);
-		}
-		for (GoTerm term : terms){
-				System.out.println(term.getId());
-				System.out.println(term.getName());
-				System.out.println(term.getNamespace());
-				System.out.println(term.getParents());
-				System.out.println();
+		    System.out.println("Exception: " + e);
 		}
 		return terms;
 	}
+	
+	public static List<GoTerm> getRoots(List<GoTerm> terms){
+		List<GoTerm> roots = new ArrayList<GoTerm>();
+		for (GoTerm term : terms){
+			if(term.getParents().isEmpty()){
+				roots.add(term);
+			}				
+		}		
+	return roots;
+	}
+	
+	public static List<GoTerm> getChildren(List<GoTerm> terms, String id){
+		List<GoTerm> children = new ArrayList<GoTerm>();
+		for (GoTerm term : terms){
+			if(term.getParents().equals(id)){
+				System.out.println("Child found!");
+			}
+ 		}
+		
+		
+	return children;
+	}
+	
+	
 
 }

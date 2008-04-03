dc04ff0fb2851c3c4650fc2f201562fa0f53605c
rdeneer <rdeneer@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-04-03 10:24:17 +0000

    Added namespace.

--- trunk/tools/project2008/GoTerm.java
+++ trunk/tools/project2008/GoTerm.java
@@ -4,11 +4,13 @@ public class GoTerm {
 
 	private String id;
 	private String name;
+	private String namespace;
 	private List<String> isa;
 	
-	public GoTerm(String id, String name, List<String> isa){
+	public GoTerm(String id, String name, String namespace, List<String> isa){
 		this.id = id;
 		this.name = name;
+		this.namespace = namespace;
 		this.isa = isa;
 	}
 	
@@ -20,11 +22,12 @@ public class GoTerm {
 		return name;	
 	}
 	
+	public String getNamespace(){
+		return namespace;	
+	}
+	
 	public List<String> getParents(){
 		return isa;
-	}
-
-	
-	
+	}	
 
 }

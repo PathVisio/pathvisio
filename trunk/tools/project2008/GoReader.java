983bb0cc8e496eef154139b719708e7b1d15ce7f
rdeneer <rdeneer@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-04-03 12:38:15 +0000

    Changed List to Set.

--- trunk/tools/project2008/GoReader.java
+++ trunk/tools/project2008/GoReader.java
@@ -1,84 +1,132 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 
 public class GoReader {
 	
-	public static void main(String[] args) {
-		List<GoTerm> terms = new ArrayList<GoTerm>();
-		List<GoTerm> roots = new ArrayList<GoTerm>();
-		List<GoTerm> children = new ArrayList<GoTerm>();
+	public static void main(String[] args){
+		// new list with GoTerms
+		Set<GoTerm> terms = new HashSet<GoTerm>();
+		// new list with roots 
+		Set<GoTerm> roots = new HashSet<GoTerm>();
+		// new list with children
+		Set<GoTerm> children = new HashSet<GoTerm>();
+		// start reading the GoDatabase
+		// make sure args[0] refers to the database file
 		terms=readGoDatabase(args[0]);
+		// get the roots of the database
 		roots=getRoots(terms);
+		// as example get the children for this id
 		String id="GO:0006854";
 		children=getChildren(terms,id);
-		for (GoTerm root : roots){
-			System.out.println(root.getId());
-			System.out.println(root.getName());
-			System.out.println(root.getNamespace());
-			System.out.println(root.getParents());
+		// print all the terms
+		for (GoTerm term : terms){
+			System.out.println(term.getId());
+			System.out.println(term.getName());
+			System.out.println(term.getNamespace());
+			System.out.println(term.getParents());
 			System.out.println();
 		}
 	}
 	
-	public static List<GoTerm> readGoDatabase(String path){
+	public static Set<GoTerm> readGoDatabase(String path){
+		// each line is read as a string
 		String line; 
-		List<GoTerm> terms = new ArrayList<GoTerm>();			
-		
-		try {
+		// the list with GoTerms
+		Set<GoTerm> terms = new HashSet<GoTerm>();			
+		// start reading the file (buffered)
+		try 
+		{
 			FileReader fr = new FileReader(path);
 			BufferedReader br = new BufferedReader(fr);
-
-			while((line=br.readLine()) != null){
-				if(line.startsWith("[Term]")){					
+			// read line-by-line until the end is reached
+			while((line=br.readLine()) != null)
+			{
+				// if the line starts with a term, process it
+				if(line.startsWith("[Term]"))
+				{
+					// extract the GO:ID
 					String id = br.readLine().substring(4);
+					// extract the name
 					String name = br.readLine().substring(6);
+					// extract the namespace
 					String namespace = br.readLine().substring(11);
-					List<String> isa = new ArrayList<String>();
+					// create a list for the partens
+					Set<String> isa = new HashSet<String>();
+					// track if the entry is marked as obsolete
 					boolean obsolete = false;
-					// continue reading lines until the end of the term is reached
-					do {
+					// continue reading lines until the end of the term or file
+					do 
+					{
 						line=br.readLine();
+						if(line == null)
+						{
+							break;
+						}
+						// if the line starts with an is_a, add this to the list
 						if(line.startsWith("is_a:")){
 							isa.add(line.substring(6,16));							
 						}
-						if (line.startsWith("is_obsolete:")){
+						// if the line starts with is_obsolete, obsolete = true
+						if (line.startsWith("is_obsolete:"))
+						{
 							obsolete = true;
 						}
 					}
 					while(! line.equals(""));
-					if (!obsolete){
+					// only add the term if it isn't obsolete
+					if (!obsolete)
+					{
 						terms.add(new GoTerm(id,name,namespace,isa));
 					}
 				}
 		    }		
-		    fr.close();
-		    }		
-		    catch(Exception e) {
-		    System.out.println("Exception: " + e);
+			fr.close();
+		}		
+		catch(Exception e) 
+		{
+			System.out.println("Exception: " + e);
+			e.printStackTrace();
 		}
+		// return the GoTerms
 		return terms;
 	}
 	
-	public static List<GoTerm> getRoots(List<GoTerm> terms){
-		List<GoTerm> roots = new ArrayList<GoTerm>();
-		for (GoTerm term : terms){
-			if(term.getParents().isEmpty()){
+	public static Set<GoTerm> getRoots(Set<GoTerm> terms)
+	{
+		// create a list for the roots
+		Set<GoTerm> roots = new HashSet<GoTerm>();
+		// walk through the terms to find the roots
+		for (GoTerm term : terms)
+		{
+			// if a term has no parents, it's a root
+			if(term.getParents().isEmpty())
+			{
+				// add the term as root
 				roots.add(term);
 			}				
-		}		
+		}
+		// return the root list
 		return roots;
 	}
 	
-	public static List<GoTerm> getChildren(List<GoTerm> terms, String id){
-		List<GoTerm> children = new ArrayList<GoTerm>();
-		for (GoTerm term : terms){
-			if(term.getParents().contains(id)){
+	public static Set<GoTerm> getChildren(Set<GoTerm> terms, String id)
+	{
+		// create a list of children, as a term can have more than 1 child
+		Set<GoTerm> children = new HashSet<GoTerm>();
+		// walk through the terms to find the children
+		for (GoTerm term : terms)
+		{
+			// if a term contains this id as parent, it must be a child
+			if(term.getParents().contains(id))
+			{
 				children.add(term);			
 			}
  		}
+		// return the list of children
 		return children;
 	}
 }

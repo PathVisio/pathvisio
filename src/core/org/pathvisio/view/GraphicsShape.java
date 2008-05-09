39022782fc8501e32636254a3ae8b87a1f47ef3e
Thomas Kelder <thomaskelder@users.noreply.github.com>
2008-05-09 15:33:21 +0000

    added function to create more handles for large objects

--- src/core/org/pathvisio/view/GraphicsShape.java
+++ src/core/org/pathvisio/view/GraphicsShape.java
@@ -355,14 +355,36 @@ public abstract class GraphicsShape extends Graphics implements LinkProvider {
 	
 	List<LinkAnchor> linkAnchors = new ArrayList<LinkAnchor>();
 	
+	private static final int MIN_SIZE_LA = 15 * 25;
+	private int num_linkanchors_h = -1;
+	private int num_linkanchors_v = -1;
+	
 	public List<LinkAnchor> getLinkAnchors() {
-		if(linkAnchors.size() == 0) {
-			linkAnchors.add(new LinkAnchor(canvas, gdata, 0, -1));
-			linkAnchors.add(new LinkAnchor(canvas, gdata, 1, 0));
-			linkAnchors.add(new LinkAnchor(canvas, gdata, 0, 1));
-			linkAnchors.add(new LinkAnchor(canvas, gdata, -1, 0));
+		//Number of link anchors depends on the size of the object
+		//If the width/height is large enough, there will be three link anchors per side,
+		//Otherwise there will be only one link anchor per side
+		int n_h = gdata.getMWidth() >= MIN_SIZE_LA ? 3 : 1;
+		int n_v = gdata.getMHeight() >= MIN_SIZE_LA ? 3 : 1;
+		if(n_h != num_linkanchors_h || n_v != num_linkanchors_v) {
+			createLinkAnchors(n_h, n_v);
 		}
 		return linkAnchors;
+	}
+	
+	private void createLinkAnchors(int n_h, int n_v) {
+		linkAnchors.clear();
+		double d_h = 2.0/(n_h + 1);
+		for(int i = 1; i <= n_h; i++) {
+			linkAnchors.add(new LinkAnchor(canvas, gdata, -1 + i * d_h, -1));
+			linkAnchors.add(new LinkAnchor(canvas, gdata, -1 + i * d_h, 1));
+		}
+		double d_v = 2.0/(n_v + 1);
+		for(int i = 1; i <= n_v; i++) {
+			linkAnchors.add(new LinkAnchor(canvas, gdata, -1, -1 + i * d_v));
+			linkAnchors.add(new LinkAnchor(canvas, gdata, 1, -1 + i * d_v));
+		}
+		num_linkanchors_h = n_h;
+		num_linkanchors_v = n_v;
 	}
 	
 	boolean showLinkAnchors = false;

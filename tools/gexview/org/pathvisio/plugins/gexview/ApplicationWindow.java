999e2319caf69913cedec0d7836c7cee851b97f6
martijn <amarillion@users.noreply.github.com>
2008-05-16 13:44:34 +0000

    Start of gex viewer app

---
+++ tools/gexview/org/pathvisio/plugins/gexview/ApplicationWindow.java
@@ -0,0 +1,107 @@
+package org.pathvisio.plugins.gexview;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.*;
+
+import org.pathvisio.Revision;
+import org.pathvisio.data.*;
+import org.pathvisio.gui.swing.StandaloneActions;
+import org.pathvisio.util.swing.SimpleFileFilter;
+
+public class ApplicationWindow 
+{
+	// main window frame
+	JFrame frame;
+	JTable tblHeatmap;
+	
+	// both are null if there is no gex opened
+	File gexFile = null;
+	SimpleGex gex = null;
+	
+	public void openGex(File f)
+	{
+		if (!f.exists()) throw new IllegalArgumentException ("File doesn't exist!");
+		gexFile = f;
+		tblHeatmap.setModel(new HeatmapTableModel(gex));
+		
+		try
+		{
+			gex = new SimpleGex(f.toString(), false, new DataDerby());
+		}
+		catch (DataException e)
+		{
+			gex = null;
+			gexFile = null;
+			JOptionPane.showConfirmDialog(frame, "Data exception", "Data exception", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	private class OpenAction extends AbstractAction
+	{
+		private static final long serialVersionUID = 1L;
+
+		public OpenAction()
+		{
+			super();
+			putValue(NAME, "Open");
+			putValue(SMALL_ICON, new ImageIcon (StandaloneActions.IMG_OPEN));
+			putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl O"));
+		}
+		
+		public void actionPerformed(ActionEvent e) 
+		{
+			JFileChooser jfc = new JFileChooser();
+			jfc.setFileFilter(new SimpleFileFilter("pgex files", "*.pgex"));
+			if (jfc.showDialog(null, "Open") == JFileChooser.APPROVE_OPTION)
+			{
+				File f = jfc.getSelectedFile();
+				openGex(f);
+			}
+		}
+	}
+	
+	private OpenAction openAction = new OpenAction();
+	
+	public JMenuBar createMenu()
+	{
+		JMenuBar result = new JMenuBar();
+		
+		JMenu file = new JMenu("File");
+		JMenu help = new JMenu("Help");
+		
+		file.add(openAction);
+		
+		result.add (file);
+		result.add (help);
+		
+		return result;
+	}
+	
+	public void createAndShowGUI()
+	{
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Gex Viewer r" + Revision.REVISION);
+		frame.setJMenuBar(createMenu());
+		
+		gexFile = new File ("/home/martijn/prg/pathvisio-trunk/example-data/sample_data_1.pgex");
+		try
+		{
+			gex = new SimpleGex (gexFile + "", 
+					false, new DataDerby());
+		}
+		catch (DataException e)
+		{
+			e.printStackTrace();
+		}
+		tblHeatmap = new JTable(new HeatmapTableModel(gex));
+		//tblHeatmap.set
+		frame.add (new JScrollPane (tblHeatmap));
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
---
+++ tools/gexview/org/pathvisio/plugins/gexview/Main.java
@@ -0,0 +1,10 @@
+package org.pathvisio.plugins.gexview;
+
+public class Main 
+{
+	public static void main (String [] args)
+	{
+		ApplicationWindow aw = new ApplicationWindow();
+		aw.createAndShowGUI();
+	}	
+}

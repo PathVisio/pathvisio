81d8e9d40a517df7fcc503db7846b751ff990a9e
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-05-16 13:44:09 +0000

    Work on gex import wizard.
    Included Wizard classes from
    http://java.sun.com/developer/technicalArticles/GUI/swing/wizard/index.html
    (with license)
    Created menu item
    
    Widgets on first two pages of wizard
    
    Removed the need for separate Page and Descriptor classes

---
+++ trunk/src/standalone/com/nexes/test/TestPanel1Descriptor.java
@@ -0,0 +1,32 @@
+package com.nexes.test;
+
+import com.nexes.wizard.*;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+
+public class TestPanel1Descriptor extends WizardPanelDescriptor {
+    
+    public static final String IDENTIFIER = "INTRODUCTION_PANEL";
+    
+    public TestPanel1Descriptor() {
+        super(IDENTIFIER);
+    }
+    
+    public Object getNextPanelDescriptor() {
+        return TestPanel2Descriptor.IDENTIFIER;
+    }
+    
+    public Object getBackPanelDescriptor() {
+        return null;
+    }
+
+	@Override
+	protected Component createContents() 
+	{
+		return new TestPanel1();
+	}  
+    
+}

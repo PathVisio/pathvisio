5d076b400cc082186a8e7b0d7d0908d1c2c1ff19
Thomas Kelder <thomaskelder@users.noreply.github.com>
2008-05-27 14:59:11 +0000

    updated lucene indexer to index cross references from synonym database

--- tools/lucene-indexer/test/org/pathvisio/indexer/Search.java
+++ tools/lucene-indexer/test/org/pathvisio/indexer/Search.java
@@ -14,17 +14,22 @@ import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
+import org.pathvisio.Engine;
+import org.pathvisio.data.GdbManager;
+import org.pathvisio.debug.Logger;
+import org.pathvisio.preferences.PreferenceManager;
 
 public class Search extends TestCase {
 	static final File pathwayDir = new File("tools/lucene-indexer/test-data");
 	static final File indexDir = new File("tools/lucene-indexer/test-index");
-
-	protected void setUp() throws Exception {
-		createIndex();
-	}
 	
-	public void createIndex() {
+	public void testCreateIndex() {
 		try {
+			//Connect to any GDB in the preferences
+			Engine.init();
+			PreferenceManager prefs = Engine.getCurrent().getPreferences();
+			prefs.load();
+			GdbManager.init();
 			CreateIndex.index(pathwayDir, indexDir);
 		} catch(Exception e) {
 			fail("Exception during indexing: " + e.getMessage());
@@ -32,23 +37,49 @@ public class Search extends TestCase {
 	}
 	
 	public void testOrganismSearch() {
-		Query query = new TermQuery(new Term("organism", "Homo sapiens"));
+		Query query = new TermQuery(
+				new Term(CreateIndex.FIELD_ORGANISM, "Homo sapiens")
+		);
 		Hits hits = query(query);
 		assertTrue("nr of hits should be > 0, is: " + hits.length(), hits.length() > 0);
 	}
 	
 	public void testDataNodeSearch() throws CorruptIndexException, IOException {
-		Query query = new TermQuery(new Term("xref.id", "8643"));
+		Query query = new TermQuery(new Term(CreateIndex.FIELD_ID, "8643"));
 		Hits hits = query(query);
 		boolean found = false;
 		for (int i = 0; i < hits.length(); i++) {
 			Document doc = hits.doc(i);
-			String name = doc.get("name");
+			String name = doc.get(CreateIndex.FIELD_NAME);
 			if("Hedgehog Signaling Pathway".equals(name)) {
 				found = true;
 			}
 		}
 		assertTrue(found);
+	}
+	
+	public void testCrossRefSearch() throws CorruptIndexException, IOException {
+		Query q1 = new TermQuery(new Term(CreateIndex.FIELD_XID, "32786_at"));
+		Query q2 = new TermQuery(new Term(CreateIndex.FIELD_XID, "GO:0003700"));
+		Hits hits = query(q1);
+		assertTrue(searchHits(hits, CreateIndex.FIELD_NAME, "Oxidative Stress"));
+		hits = query(q2);
+		assertTrue(searchHits(hits, CreateIndex.FIELD_NAME, "Oxidative Stress"));
+	}
+	
+	boolean searchHits(Hits hits, String field, String result) throws CorruptIndexException, IOException {
+		boolean found = false;
+		Logger.log.info("Searching hits for: " + result + " in " + field);
+		Logger.log.info("\tNr hits: " + hits.length());
+		for (int i = 0; i < hits.length(); i++) {
+			Document doc = hits.doc(i);
+			String value = doc.get(field);
+			Logger.log.trace("\tfound: " + value);
+			if(result.equals(value)) {
+				found = true;
+			}
+		}
+		return found;
 	}
 	
 	public Hits query(Query q) {
@@ -65,7 +96,10 @@ public class Search extends TestCase {
 	
 	public Hits query(String q) {
 		try {
-			QueryParser parser = new QueryParser("name", new StandardAnalyzer());
+			QueryParser parser = new QueryParser(
+					CreateIndex.FIELD_NAME, 
+					new StandardAnalyzer()
+			);
 			Query query = parser.parse(q);
 			return query(query);
 		} catch(Exception e) {

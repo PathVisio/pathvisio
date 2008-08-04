c27451d15cb6af70bbbd14114dae3a52e374f56a
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-08-04 15:43:10 +0000

    fixed #597

--- trunk/plugins/visualization/org/pathvisio/visualization/plugins/VisualizationPlugin.java
+++ trunk/plugins/visualization/org/pathvisio/visualization/plugins/VisualizationPlugin.java
@@ -16,11 +16,23 @@
 //
 package org.pathvisio.visualization.plugins;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import org.pathvisio.data.GexManager;
+import org.pathvisio.data.GexManager.GexManagerEvent;
+import org.pathvisio.data.GexManager.GexManagerListener;
+import org.pathvisio.debug.Logger;
+import org.pathvisio.gui.swing.MainPanel;
+import org.pathvisio.gui.swing.SwingEngine;
 import org.pathvisio.plugin.Plugin;
 import org.pathvisio.visualization.Visualization;
+import org.pathvisio.visualization.VisualizationManager;
 import org.pathvisio.visualization.VisualizationMethod;
 import org.pathvisio.visualization.VisualizationMethodProvider;
 import org.pathvisio.visualization.VisualizationMethodRegistry;
+import org.pathvisio.visualization.gui.VisualizationDialog;
 
 /**
  * Plugin that registers several visualization methods
@@ -56,6 +68,35 @@ public class VisualizationPlugin implements Plugin {
 					}
 			}
 		);
+		//Register the menu items
+		SwingEngine.getCurrent().registerMenuAction ("Data", new VisualizationAction(
+				SwingEngine.getCurrent().getApplicationPanel())
+		);
 	}
 
+	public static class VisualizationAction extends AbstractAction implements GexManagerListener {
+		MainPanel mainPanel;
+		
+		public VisualizationAction(MainPanel mainPanel) {
+			putValue(NAME, "Visualization options");
+			this.mainPanel = mainPanel;
+			setEnabled(GexManager.getCurrent().isConnected());
+			GexManager.getCurrent().addListener(this);
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			new VisualizationDialog(
+					VisualizationManager.getCurrent(),
+					SwingEngine.getCurrent().getFrame(),
+					mainPanel
+			).setVisible(true);
+		}
+
+		public void gexManagerEvent(GexManagerEvent e) 
+		{
+			boolean isConnected = GexManager.getCurrent().isConnected();
+			Logger.log.trace("Visualization options action, gexmanager event, connected: " + isConnected);
+			setEnabled(isConnected);
+		}
+	}
 }

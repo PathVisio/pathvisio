c50b15ba994076c7de9157e5fb428d57e05e0219
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-08-04 18:10:02 +0000

    Added dist-clean target for extra cleanliness, simplified gpmldiff and converter scripts

--- trunk/gpmldiff.sh
+++ trunk/gpmldiff.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-source classpath.sh;
-
-MYCLASSPATH1=$PATHVISIO_CP:build/v1:build/core
-
-java -ea -cp $MYCLASSPATH1 org.pathvisio.gpmldiff.GpmlDiff $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -ea -classpath pathvisio_core.jar org.pathvisio.gpmldiff.GpmlDiff $@

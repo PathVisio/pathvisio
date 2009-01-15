6ba396c65237b537f5d8cf5c6260fb1d99124859
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2009-01-15 13:51:12 +0000

    added undo/redo to description applet (fixes #341)

--- trunk/src/wikipathways/org/pathvisio/gui/wikipathways/DescriptionApplet.java
+++ trunk/src/wikipathways/org/pathvisio/gui/wikipathways/DescriptionApplet.java
@@ -18,12 +18,21 @@ package org.pathvisio.gui.wikipathways;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.Document;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 
 import org.pathvisio.model.Pathway;
 import org.pathvisio.model.PathwayElement;
@@ -74,6 +83,49 @@ public class DescriptionApplet extends PathwayPageApplet {
 			}
 		});
 		
+		//Add an undo manager
+		final UndoManager undo = new UndoManager();
+		Document doc = text.getDocument();
+
+		// Listen for undo and redo events
+		doc.addUndoableEditListener(new UndoableEditListener() {
+			public void undoableEditHappened(UndoableEditEvent evt) {
+				undo.addEdit(evt.getEdit());
+			}
+		});
+
+		// Create an undo action and add it to the text component
+		text.getActionMap().put("Undo",
+				new AbstractAction("Undo") {
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					if (undo.canUndo()) {
+						undo.undo();
+					}
+				} catch (CannotUndoException e) {
+				}
+			}
+		});
+
+		// Bind the undo action to ctl-Z
+		text.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "Undo");
+
+		// Create a redo action and add it to the text component
+		text.getActionMap().put("Redo",
+				new AbstractAction("Redo") {
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					if (undo.canRedo()) {
+						undo.redo();
+					}
+				} catch (CannotRedoException e) {
+				}
+			}
+		});
+
+		// Bind the redo action to ctl-Y
+		text.getInputMap().put(KeyStroke.getKeyStroke("control Y"), "Redo");
+
 		Container content = getContentPane();
 		content.add(new JScrollPane(text), BorderLayout.CENTER);
 	}

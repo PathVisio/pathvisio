5a124406960db70e3a211f8c91e90ed92a2729bc
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2009-01-19 14:27:41 +0000

    Exclude webservice client from daily build

--- trunk/tools/dailybuild/add_license_header.sh
+++ trunk/tools/dailybuild/add_license_header.sh
@@ -3,9 +3,10 @@
 # extract the license header from a file that has it:
 head -n 16 ../../src/core/org/pathvisio/Engine.java > license.txt
 # create a list of files missing the license header (exclude Revision.java)
-find ../.. -name "*.java" ! -name "Revision.java" \
- | xargs -d '\n' grep -L "Apache License, Version 2.0" \
- | xargs -d '\n' grep -l "package org.pathvisio" \
+find ../.. -wholename "**/org/pathvisio/**/*.java" \
+	! -name "Revision.java" \
+	! -wholename "**/src-axis-wpclient/**" \
+	| xargs -d '\n' grep -L "Apache License, Version 2.0" \
   > license_check.txt
 # add contents of license.txt at the beginning of those files.
 for i in $( cat license_check.txt ); do

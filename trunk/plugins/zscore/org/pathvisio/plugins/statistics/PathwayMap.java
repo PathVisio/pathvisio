023f2c191675f80e9b5383884bfe33d68779239a
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2009-02-27 14:31:02 +0000

    Added methods to calculate ZScore in the exact same way as MAPPFinder. Started work on Permutation test, unfinished

---
+++ trunk/plugins/zscore/org/pathvisio/plugins/statistics/PathwayMap.java
@@ -0,0 +1,106 @@
+// PathVisio,
+// a tool for data visualization and analysis using Biological Pathways
+// Copyright 2006-2009 BiGCaT Bioinformatics
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+//  
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+package org.pathvisio.plugins.statistics;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.pathvisio.debug.Logger;
+import org.pathvisio.model.Xref;
+import org.pathvisio.model.XrefWithSymbol;
+import org.pathvisio.util.FileUtils;
+import org.pathvisio.util.PathwayParser;
+import org.pathvisio.util.PathwayParser.ParseException;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+/**
+ * A collection of pathways, 
+ * parsed quickly using PathwayParser
+ */
+public class PathwayMap 
+{
+	public static class PathwayInfo
+	{
+		Set<Xref> srcRefs;
+		String name;
+		File file;
+	}
+
+	List<PathwayInfo> pathways;	
+	
+	PathwayMap(File pwDir)
+	{
+		try
+		{
+			xmlReader = XMLReaderFactory.createXMLReader();
+		}
+		catch (SAXException e)
+		{
+			Logger.log.error("Problem while searching pathways", e);
+			throw new IllegalStateException(); // TODO: more info in exception
+		}
+		
+		doGetSrcRefs(pwDir);
+	}
+	
+	private XMLReader xmlReader = null;
+
+	private void doGetSrcRefs(File pwDir)
+	{
+		pathways = new ArrayList<PathwayInfo>();
+		
+		for (File file : FileUtils.getFiles(pwDir, "gpml", true))
+		{
+			try
+			{
+				PathwayParser pwyParser = new PathwayParser(file, xmlReader);
+				
+				Logger.log.info ("Reading references from " + pwyParser.getName());
+				
+				Set<Xref> srcRefs = new HashSet<Xref>();
+				for (XrefWithSymbol x : pwyParser.getGenes()) srcRefs.add (x.asXref());
+				
+				PathwayInfo pi = new PathwayInfo();
+				pi.name = pwyParser.getName();
+				pi.srcRefs = srcRefs;
+				pi.file = file;
+				pathways.add (pi);
+			}
+			catch (ParseException ex)
+			{
+				// ignore files that are not valid gpml, just skip
+			}
+		}
+	}
+	
+
+	public Set<Xref> getSrcRefs()
+	{
+		Set<Xref> result = new HashSet<Xref>();
+		for (PathwayInfo pi : pathways)
+		{
+			result.addAll (pi.srcRefs);
+		}
+		return result;
+	}
+}
--- trunk/plugins/zscore/org/pathvisio/plugins/statistics/ZScoreCalculator.java
+++ trunk/plugins/zscore/org/pathvisio/plugins/statistics/ZScoreCalculator.java
@@ -18,8 +18,10 @@ package org.pathvisio.plugins.statistics;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.pathvisio.data.DataException;
 import org.pathvisio.data.Gdb;
@@ -27,17 +29,11 @@ import org.pathvisio.data.ReporterData;
 import org.pathvisio.data.SimpleGex;
 import org.pathvisio.debug.Logger;
 import org.pathvisio.model.Xref;
-import org.pathvisio.model.XrefWithSymbol;
-import org.pathvisio.util.FileUtils;
-import org.pathvisio.util.PathwayParser;
-import org.pathvisio.util.PathwayParser.ParseException;
 import org.pathvisio.util.ProgressKeeper;
 import org.pathvisio.util.Stats;
 import org.pathvisio.visualization.colorset.Criterion;
 import org.pathvisio.visualization.colorset.Criterion.CriterionException;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
+import org.pathvisio.plugins.statistics.PathwayMap.PathwayInfo;
 
 /**
  * Calculates statistics on a set of Pathways, either step by step with intermediate results,
@@ -45,11 +41,12 @@ import org.xml.sax.helpers.XMLReaderFactory;
  */
 public class ZScoreCalculator 
 {			
+	private PathwayMap pwyMap;
+	private Map<Xref, RefInfo> dataMap;
 	private final StatisticsResult result;
 	private final ProgressKeeper pk;
-	private Iterator<File> iterator = null;
-	private int i = 0;
-	private List<File> files;
+	private Map<PathwayInfo, StatisticsPathwayResult> statsMap = 
+		new HashMap<PathwayInfo, StatisticsPathwayResult>();
 	
 	public ZScoreCalculator(Criterion crit, File pwDir, SimpleGex gex, Gdb gdb, ProgressKeeper pk)
 	{
@@ -67,95 +64,161 @@ public class ZScoreCalculator
 		result.gex = gex;
 		result.gdb = gdb;
 		this.pk = pk;
-
-		try
-		{
-			xmlReader = XMLReaderFactory.createXMLReader();
-		}
-		catch (SAXException e)
-		{
-			Logger.log.error("Problem while searching pathways", e);
-			throw new IllegalStateException(); // TODO: more info in exception
-		}
 	}
 
-	public Iterator<File> getIterator()
-	{
-		if (iterator == null)
-		{
-			// first we calculate N and R
-			doCalculateTotals();
-			if (pk != null && pk.isCancelled()) return null;
-			
-			if (pk != null) pk.setTaskName("Creating pathway list");
-			
-			// now we calculate n and r for each pwy				
-			files = FileUtils.getFiles(result.pwDir, "gpml", true);
-			iterator = files.iterator();								
-		}
-		return iterator;
-	}
-	
-	public boolean hasNext()
-	{
-		return getIterator().hasNext();
-	}
-	
 	/**
-	 * @return May be null if the result could not be calculated, 
-	 * e.g. if a Parser Exception occurred
+	 * We have two slightly different methods for calculating zscores:
+	 * MappFinder and Alternative
+	 * 
+	 * This base class abstracts the difference out so we can easily select
+	 * one of the two methods.
 	 */
-	public StatisticsPathwayResult next()
+	private abstract class Method
 	{
-		File f = getIterator().next();
-		i++;
-		if (pk != null)
-		{
-			pk.setProgress((int)((0.2 + (0.8 * (double)i / (double)files.size())) * 100.0));
-		}
+		/**
+		 * calculate result.bigN and result.bigR
+		 */
+		public abstract void calculateTotals () 
+			throws DataException;
+		
+		/**
+		 * Do a permutation test to calculate permP and adjP
+		 */
+		public abstract void permute();
+	}
 
-		StatisticsPathwayResult spr = doCalculatePathway(f);
-		if (spr != null)
+	/**
+	 * calculate n and r for a single pathway.
+	 * 
+	 * dataMap should already have been initialized
+	 */
+	private StatisticsPathwayResult calculatePathway(PathwayInfo pi) 
+	{
+		int cPwyMeasured = 0;
+		int cPwyPositive = 0;
+		int cPwyTotal = pi.srcRefs.size();
+		
+		for (Xref ref : pi.srcRefs)
 		{
-			result.stm.addRow (spr);
-			result.stm.sort();
+			RefInfo refInfo = dataMap.get(ref);
+			if (refInfo.isMeasured()) cPwyMeasured++;
+			if (refInfo.isPositive()) cPwyPositive++;
 		}
 		
+		double zscore = Stats.zscore(cPwyMeasured, cPwyPositive, result.bigN, result.bigR); 
+		StatisticsPathwayResult spr = new StatisticsPathwayResult(
+				pi.file, pi.name,
+				cPwyMeasured, cPwyPositive, cPwyTotal, zscore);
 		return spr;
 	}
 	
 	/**
-	 * call only after hsaNext() returns false
+	 * Information about the result of evaluating a criterion on a xref.
+	 * 
+	 * A given xref can have 0 or more measured probes associated with it,
+	 * and each measured probe can be positive or not. 
 	 */
-	public StatisticsResult getResult()
+	private static class RefInfo
 	{
-		assert (iterator != null && !iterator.hasNext());
-		return result;
-	}
-
-	/** calculate everything at once */
-	public StatisticsResult calculate()
-	{
-		int i = 0;
-		for (Iterator<File> it = getIterator(); it.hasNext(); )
+		final int probesMeasured;
+		final int probesPositive;
+		
+		/**
+		 * Initialize. 
+		 * @param aProbesMeasured must be >= 0
+		 * @param aProbesPostive must be >= 0, and <= aProbesMeasured.
+		 */
+		RefInfo(int aProbesMeasured, int aProbesPositive)
 		{
-			File f = getIterator().next();
-			if (pk != null)
-			{
-				pk.setProgress((int)((0.2 + (0.8 * (double)i / (double)files.size())) * 100.0));
-			}
-			StatisticsPathwayResult spr = doCalculatePathway(f);
-			if (spr != null) result.stm.addRow (spr);
+			probesMeasured = aProbesMeasured;
+			probesPositive = aProbesPositive;
+			if (probesMeasured < 0) throw new IllegalArgumentException();
+			if (probesPositive < 0) throw new IllegalArgumentException();
+			if (probesPositive > probesMeasured) throw new IllegalArgumentException();
 		}
-		result.stm.sort();
-		return result;
+
+		/**
+		 * Calculate the positive fraction of probes. 
+		 * E.g if 2 out of 3 probes are positive, count only 2/3.
+		 * This is not the method Used by MAPPFinder.
+		 */
+		double getPositiveFraction()
+		{
+			return (double)probesPositive / (double)probesMeasured;
+		}
+		
+		/** 
+		 * returns true if probesPositive > 0, meaning that at
+		 * least one of the probes is measured and positive.
+		 * 
+		 * If probesMeasured is false, this will be false too.
+		 * 
+		 * This is a very optimistic way of calling a ref positive, because there could be
+		 * 1000 non-positive probes. But this is the method used by MAPPFinder.
+		 * 
+		 * For an alternative way, check getPositiveFraction
+		 */
+		boolean isPositive()
+		{
+			return probesPositive > 0;
+		}
+		
+		/**
+		 * returns true if at least one probe is measured
+		 */
+		boolean isMeasured()
+		{
+			return probesMeasured > 0;
+		}		
 	}
+	
 	/**
-	 * calculate bigN and bigR
+	 * Checks if the given ref evaluates positive for the criterion
+	 * 
+	 * Assumes that ref has already been cached earlier in a call to
+	 * result.gex.cacheData(...)
 	 */
-	private void doCalculateTotals()
+	private RefInfo evaluateRef (Xref srcRef)
 	{
-		try
+		int cGeneTotal = 0;
+		int cGenePositive = 0;
+		List<ReporterData> rows = result.gex.getCachedData().getData(srcRef);
+		
+		if (rows != null)
+		{
+			cGeneTotal = rows.size();			
+			for (ReporterData row : rows)
+			{
+				if (pk != null && pk.isCancelled()) return null;
+				try
+				{	
+					boolean eval = result.crit.evaluate(row.getByName());
+					if (eval) cGenePositive++;
+				}
+				catch (CriterionException e)
+				{
+					Logger.log.error ("Unknown error during statistics", e);
+				}
+			}
+		
+		}
+		
+		return new RefInfo (cGeneTotal, cGenePositive);
+	}
+	
+	/**
+	 * Implementation of the Alternative method for calculating zscores.
+	 * This takes the whole dataset into account.
+	 */
+	private class AlternativeMethod extends Method
+	{
+		@Override
+		/**
+		 * calculate bigN and bigR, based on the dataset.
+		 * This goes through every row of the dataset and counts the number
+		 * of total rows (bigN) and the number of rows meeting our criterion (bigR)
+		 */
+		public void calculateTotals() throws DataException
 		{
 			int maxRow = result.gex.getMaxRow();
 			for (int i = 0; i < maxRow; ++i)
@@ -170,7 +233,6 @@ public class ZScoreCalculator
 					{
 						result.bigR++;
 					}		
-//							Logger.log.trace ("Row " + i +  " (" + d.getXref() + ") = " + result);
 				}
 				catch (CriterionException e)
 				{
@@ -180,96 +242,211 @@ public class ZScoreCalculator
 				if (pk != null) pk.setProgress ((int)(0.2 * (double)i / (double)maxRow * 100.0));
 			}
 		}
-		catch (DataException e)
-		{
-			Logger.log.error ("Problem during calculation of R/N ", e);
-			//TODO: better error handling
-		}
 
-		Logger.log.info ("N: " + result.bigN + ", R: " + result.bigR);
+		@Override
+		public void permute() 
+		{
+			//TODO: currently only implemented for MappFinderMethod.
+			// adjP and permP will be 0 for all pathways
+		}
 	}
 	
 	/**
-	 * @return returns null if the pathway could not be parsed 
+	 * Implementation of the MAPPFinder method for calculating zscores.
+	 * This takes only the part of the dataset into account that maps to pathways.
 	 */
-	private StatisticsPathwayResult doCalculatePathway(File file)
-	{
-		try
+	private class MappFinderMethod extends Method
+	{		
+		/**
+		 * Shuffle the values of the map,
+		 * so that each K, V pair is (likely) broken up,
+		 * each K will (likely) get a new V.
+		 * 
+		 * @param map: the map to be permuted. This value is modified directly.
+		 */
+		private <K, V> void permuteMap (Map<K, V> map)
 		{
-			if (pk != null) pk.setTaskName("Analyzing " + file.getName());
+			List<V> values = new ArrayList<V>();
+			values.addAll (map.values());
+			Collections.shuffle (values);
 			
-			PathwayParser pwyParser = new PathwayParser(file, xmlReader);
-			
-			Logger.log.info ("Calculating statistics for " + pwyParser.getName());
-			
-			List <Xref> srcRefs = new ArrayList<Xref>();
-			for (XrefWithSymbol x : pwyParser.getGenes()) srcRefs.add (x.asXref());
-			
-			try
+			int i = 0;
+			for (K key : map.keySet())
 			{
-				result.gex.cacheData(srcRefs, new ProgressKeeper(1000), result.gdb);
+				map.put (key, values.get(i));
+				i++;
 			}
-			catch (DataException e)
-			{
-				Logger.log.error ("Exception while caching data", e);
-			}
-
-			int cPwyTotal = srcRefs.size();
-			int cPwyMeasured = 0;
+		}
+		
+		/**
+		 * Perform a permutation test and calculate PermuteP values.
+		 *
+		 * permutes the data 1000 times while keeping the labels fixed.
+		 * Calculate the rank of the actual zscore compared to the permuted zscores.
+		 * Two-tailed test, so checks for very low z-scores as well as very high z-scores.
+		 */
+		public void permute()
+		{
+			// create a deep copy of dataMap.
+			Map<Xref, RefInfo> dataMap2 = new HashMap<Xref, RefInfo>();
+			for (Xref key : dataMap.keySet()) { dataMap2.put (key, dataMap.get(key)); }
 			
-			double cPwyPositive = 0;
+			// we count the number of times a zscore is extremer,
+			// i.e. further away from 0 than the actual zscore.
+			Map<PathwayInfo, Integer> extremer = new HashMap<PathwayInfo, Integer>();
 			
-			for (Xref srcRef : srcRefs)
+			for (PathwayInfo pi : pwyMap.pathways) extremer.put (pi, 0);
+			
+			for (int i = 0; i < 999; ++i)
 			{
-				if (pk != null && pk.isCancelled()) return null;
-				
-				List<ReporterData> rows = result.gex.getCachedData().getData(srcRef);
-				
-				if (rows != null)
+				permuteMap (dataMap2);
+		
+				for (PathwayInfo pi : pwyMap.pathways)
 				{
-					int cGeneTotal = rows.size();
-					if (cGeneTotal > 0) { cPwyMeasured++; }
-					int cGenePositive = 0;
+					int cPwyMeasured = 0;
+					int cPwyPositive = 0;
 					
-					for (ReporterData row : rows)
+					for (Xref ref : pi.srcRefs)
 					{
-						if (pk != null && pk.isCancelled()) return null;
-						try
-						{	
-							boolean eval = result.crit.evaluate(row.getByName());
-							if (eval) cGenePositive++;
-						}
-						catch (CriterionException e)
-						{
-							Logger.log.error ("Unknown error during statistics", e);
-						}
+						RefInfo refInfo = dataMap2.get(ref);
+						if (refInfo.isMeasured()) cPwyMeasured++;
+						if (refInfo.isPositive()) cPwyPositive++;
 					}
-				
-					// Map the rows back to the corresponding genes. 
-					// "yes" is counted, weighed by the # of rows per gene. 
-					// This is our "r".
+					double zscore = Stats.zscore(cPwyMeasured, cPwyPositive, result.bigN, result.bigR); 
 					
-					//This line is different from MAPPFinder: if 2 out of 3 probes are positive, count only 2/3
-//					cPwyPositive += (double)cGenePositive / (double)cGeneTotal;
-					
-					//The line below is the original MAPPFinder behaviour: 
-					//  count as fully positive if at least one probe is positive
-					if (cGenePositive > 0) cPwyPositive += 1.0;
+					// compare absolutes -> two-tailed test
+					if (Math.abs(zscore) > Math.abs((statsMap.get(pi)).getZScore()))
+					{
+						extremer.put (pi, extremer.get(pi) + 1);
+					}					
 				}
 			}
 			
-			double z = Stats.zscore (cPwyMeasured, cPwyPositive, result.bigN, result.bigR);						
-			
-			StatisticsPathwayResult sr = new StatisticsPathwayResult (file, pwyParser.getName(), cPwyMeasured, (int)Math.round (cPwyPositive), cPwyTotal, z);
-			return sr;
+			// report p-vals
+			for (PathwayInfo pi : pwyMap.pathways)
+			{
+				double pval = (double)extremer.get(pi) / 1000.0;
+				StatisticsPathwayResult spr = statsMap.get(pi);
+				spr.permP = pval;
+				System.out.println (spr.getProperty(Column.PATHWAY_NAME) + "\t" + spr.getZScore() + "\t" + pval);
+			}
 		}
-		catch (ParseException pe)
+		
+		/**
+		 * calculate bigN and bigR. This only takes the part of the dataset
+		 * that maps to pathways, which leads to a calculation very similar to
+		 * MAPPFinder.
+		 */
+		@Override
+		public void calculateTotals() 
 		{
-			Logger.log.warn ("Could not parse " + file + ", ignoring", pe);
-			return null;
+			// go over all datanodes in all pathways
+			for (Xref ref : dataMap.keySet())
+			{		
+				RefInfo refInfo = dataMap.get(ref);
+				if (refInfo.isMeasured()) result.bigN++;
+				if (refInfo.isPositive()) result.bigR++;
+			}		
 		}
+	
 	}
 	
-	private XMLReader xmlReader = null;
+	private void calculateDataMap()
+	{
+		dataMap = new HashMap<Xref, RefInfo>();
+		// go over all datanodes in all pathways
+		for (Xref srcRef : pwyMap.getSrcRefs())
+		{			
+			if (pk != null && pk.isCancelled()) return;					
+			RefInfo refInfo = evaluateRef (srcRef);				
+			dataMap.put (srcRef, refInfo);			
+		}		
+	}
 	
+	private StatisticsResult calculate(Method m) throws DataException
+	{
+		// read all pathways
+		if (pk != null) 
+		{
+			if (pk.isCancelled()) return null;
+			pk.setTaskName("Creating pathway list");
+			pk.setProgress(0);
+		}
+		pwyMap = new PathwayMap (result.pwDir);
+
+		// cache data for all pathways at once.
+		if (pk != null) 
+		{
+			if (pk.isCancelled()) return null;
+			pk.setTaskName("Reading dataset");
+			pk.setProgress(20);
+		}
+		result.gex.cacheData(pwyMap.getSrcRefs(), null, result.gdb);
+		
+		// calculate dataMap
+		if (pk != null) 
+		{
+			if (pk.isCancelled()) return null;
+			pk.setTaskName("Calculating expression data");
+			pk.setProgress(40);
+		}
+		calculateDataMap();
+		
+		if (pk != null) 
+		{
+			if (pk.isCancelled()) return null;
+			pk.setTaskName("Calculating on dataset");
+			pk.setProgress(60);
+		}
+		m.calculateTotals();
+		Logger.log.info ("N: " + result.bigN + ", R: " + result.bigR);
+		
+		int i = 0;
+		for (PathwayInfo pi : pwyMap.pathways)
+		{
+			if (pk != null)
+			{
+				if (pk.isCancelled()) return null;
+				pk.setTaskName("Analyzing " + pi.file.getName());
+				pk.setProgress((int)((0.6 + (0.2 * (double)i / (double)pwyMap.pathways.size())) * 100.0));
+			}
+			StatisticsPathwayResult spr = calculatePathway(pi);
+			statsMap.put (pi, spr);
+			if (spr != null) result.stm.addRow (spr);
+		}
+		
+		if (pk != null) 
+		{
+			if (pk.isCancelled()) return null;
+			pk.setTaskName("Calculating permutation P values");
+			pk.setProgress(80);
+		}
+		m.permute();
+
+		result.stm.sort();
+		if (pk != null) 
+		{
+			pk.setProgress (100);
+			pk.setTaskName("Done");
+		}
+		return result;
+	}
+	
+	/** 
+	 * calculate StatisticsResult, using
+	 * the alternative method (not used by MappFinder)
+	 * 
+	 * This alternative method includes the whole dataset into the calculation
+	 * of the N and R parameters for the zscore, not just the part
+	 * of the dataset that maps to Pathways.
+	 */
+	public StatisticsResult calculateAlternative() throws DataException
+	{
+		return calculate (new AlternativeMethod());
+	}
+
+	public StatisticsResult calculateMappFinder() throws DataException
+	{
+		return calculate (new MappFinderMethod());
+	}		
 }

f4c113541624aaed564d627f371200a0eb02f957
Thomas Kelder <thomaskelder@users.noreply.github.com>
2009-04-22 13:17:42 +0000

    added FileUtils method to recursively get files without filtering extension

--- src/core/org/pathvisio/util/FileUtils.java
+++ src/core/org/pathvisio/util/FileUtils.java
@@ -37,7 +37,37 @@ import org.pathvisio.debug.Logger;
 /**
  * Collection of static utility methods dealing with files.
  */
-public class FileUtils {	
+public class FileUtils {
+	/**
+	 * Get all files in a directory
+	 * @param directory	The directory to get the files from
+	 * @param recursive	Whether to include subdirectories or not
+	 * @return A list of files in the given directory
+	 */
+	public static List<File> getFiles(File directory, boolean recursive) {
+		List<File> fileList = new ArrayList<File>();
+		
+		if(!directory.isDirectory()) { //File is not a directory, return file itself (if has correct extension)
+			fileList.add(directory);
+			return fileList;
+		}
+		
+		//Get all files in this directory
+		File[] files = directory.listFiles();
+
+		//Recursively add the files
+		for(File f : files) 
+		{
+			if(f.isDirectory()) 
+			{
+				if (recursive) fileList.addAll(getFiles(f, true));
+			}			
+			else fileList.add(f);
+		}
+		
+		return fileList;
+	}
+	
 	/**
 	 * Get all files in a directory
 	 * @param directory	The directory to get the files from

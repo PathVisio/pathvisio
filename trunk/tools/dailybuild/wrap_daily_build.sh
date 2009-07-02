089e662c3515b529934b8daa3b563c4ae86ae2bd
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2009-07-02 12:31:13 +0000

    License headers. Removed some old files in build system.

--- trunk/tools/dailybuild/wrap_daily_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Make sure we run with correct java version
-export JAVA_HOME=/usr/lib/jvm/java-1.5.0-sun
-export JAVA_JAVACMD=/usr/lib/jvm/java-1.5.0-sun/jre/bin/java 
-./pathvisio_daily_build.pl 
-

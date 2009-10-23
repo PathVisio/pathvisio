0eb2fbe2464c6462e0410da744a9fcface86b812
Thomas Kelder <thomaskelder@users.noreply.github.com>
2009-10-23 12:02:20 +0000

    fix for #1017 including junit testcase

---
+++ test/zscore/org/pathvisio/plugins/statistics/ZScoreTest.java
@@ -0,0 +1,99 @@
+package org.pathvisio.plugins.statistics;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.bridgedb.BridgeDb;
+import org.bridgedb.IDMapper;
+import org.bridgedb.bio.BioDataSource;
+import org.pathvisio.gex.GexManager;
+import org.pathvisio.preferences.PreferenceManager;
+import org.pathvisio.visualization.colorset.Criterion;
+
+public class ZScoreTest extends TestCase {
+	static final File PATHWAY_DIR = new File("testData/zscore/gpml");
+	static final File DATASET_FILE = new File("testData/zscore/data/Myometrium_input.pgex");
+	
+	//TODO: replace this with web service
+	static final File IDMAPPER_FILE = new File("testData/zscore/data/idmapper.bridge");
+	
+	static final String CRITERION = "[24hrs-v14 fold]  > 1.2";
+	static final File TEST_PATHWAY = new File(PATHWAY_DIR, "testAlternative.gpml");
+	
+	GexManager gexMgr;
+	IDMapper idMapper;
+	
+	@Override
+	protected void setUp() throws Exception {
+		PreferenceManager.init();
+		BioDataSource.init();
+		
+		//Load the expression data
+		gexMgr = new GexManager();
+		gexMgr.setCurrentGex(DATASET_FILE.getAbsolutePath(), false);
+		
+		Class.forName("org.bridgedb.rdb.IDMapperRdb");
+		idMapper = BridgeDb.connect("idmapper-pgdb:" + IDMAPPER_FILE.getAbsolutePath());
+	}
+	
+	public void testAlternative() {
+		try {
+			Criterion crit = new Criterion();
+			crit.setExpression(CRITERION, gexMgr.getCurrentGex().getSampleNames());
+			ZScoreCalculator calc = new ZScoreCalculator(crit, PATHWAY_DIR, gexMgr.getCachedData(), idMapper, null);
+			StatisticsResult result = calc.calculateAlternative();
+			
+			assertEquals("bigN doesn't equal number rows in data", gexMgr.getCurrentGex().getNrRow(), result.getBigN());
+			
+			//Check the pathway with known results
+			StatisticsPathwayResult pathRes = null;
+			for(StatisticsPathwayResult r : result.getPathwayResults()) {
+				if(r.getFile().equals(TEST_PATHWAY)) {
+					pathRes = r;
+					break;
+				}
+			}
+			result.getPathwayResults();
+			int n = Integer.parseInt(pathRes.getProperty(Column.N));
+			int nE = 9; //8 probes measured
+			int r = Integer.parseInt(pathRes.getProperty(Column.R));
+			int rE = 2; //2 measured probes significant
+			assertEquals("expected n = " + nE + ", got n = " + n, nE, n);
+			assertEquals("expected r = " + rE + ", got r = " + r, rE, r);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testMAPPFinder() {
+		try {
+			Criterion crit = new Criterion();
+			crit.setExpression(CRITERION, gexMgr.getCurrentGex().getSampleNames());
+			ZScoreCalculator calc = new ZScoreCalculator(crit, PATHWAY_DIR, gexMgr.getCachedData(), idMapper, null);
+			StatisticsResult result = calc.calculateMappFinder();
+			
+			//Check the pathway with known results
+			StatisticsPathwayResult pathRes = null;
+			for(StatisticsPathwayResult r : result.getPathwayResults()) {
+				if(r.getFile().equals(TEST_PATHWAY)) {
+					pathRes = r;
+					break;
+				}
+			}
+			result.getPathwayResults();
+			int n = Integer.parseInt(pathRes.getProperty(Column.N));
+			int nE = 5; //5 genes measured
+			int r = Integer.parseInt(pathRes.getProperty(Column.R));
+			int rE = 2; //2 measured genes significant
+			assertEquals("expected n = " + nE + ", got n = " + n, nE, n);
+			assertEquals("expected r = " + rE + ", got r = " + r, rE, r);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+}

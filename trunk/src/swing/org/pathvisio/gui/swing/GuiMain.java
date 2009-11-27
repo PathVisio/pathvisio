0ccf87042b2be03cb70df7416a4de5c694923ac9
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2009-11-27 09:09:42 +0000

    Bumped version no to 2.0.1

--- trunk/src/swing/org/pathvisio/gui/swing/GuiMain.java
+++ trunk/src/swing/org/pathvisio/gui/swing/GuiMain.java
@@ -365,7 +365,7 @@ public class GuiMain implements GdbEventListener, GexManagerListener
 		PreferenceManager.init();
 		Engine engine = new Engine();
 		initLog(engine);
-		engine.setApplicationName("PathVisio 2");
+		engine.setApplicationName("PathVisio 2.0.1");
 		if (PreferenceManager.getCurrent().getBoolean(GlobalPreference.USE_SYSTEM_LOOK_AND_FEEL))
 		{
 			try {

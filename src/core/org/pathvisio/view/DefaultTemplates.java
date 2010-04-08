a48fa3c5c72d359c54e95aa6231fc7ba5473411e
Alexander Pico <apico@gladstone.ucsf.edu>
2010-04-08 02:40:21 +0000

    fix #1158

--- src/core/org/pathvisio/view/DefaultTemplates.java
+++ src/core/org/pathvisio/view/DefaultTemplates.java
@@ -13,7 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 package org.pathvisio.view;
 
 import java.awt.Color;
@@ -73,6 +72,9 @@ public abstract class DefaultTemplates {
 
 		public URL getIconLocation() {
 			return  Resources.getResourceURL("new" + getName().toLowerCase() + ".gif");
+		}
+
+		public void postInsert(PathwayElement[] newElements) {
 		}
 	}
 
@@ -278,6 +280,9 @@ public abstract class DefaultTemplates {
 
 		public URL getIconLocation() {
 			return  Resources.getResourceURL("new" + getName().toLowerCase() + ".gif");
+		}
+
+		public void postInsert(PathwayElement[] newElements) {
 		}
 	}
 
--- src/core/org/pathvisio/view/Template.java
+++ src/core/org/pathvisio/view/Template.java
@@ -13,7 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 package org.pathvisio.view;
 
 import java.net.URL;
@@ -62,4 +61,7 @@ public interface Template {
 	 * @return The icon location, or null if the template doesn't have an icon
 	 */
 	URL getIconLocation();
+
+
+	void postInsert(PathwayElement[] newElements);
 }

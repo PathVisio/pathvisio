049bbeef16de0aeee12cf8afec7f2b4df2ca4a11
Alexander Pico <apico@gladstone.ucsf.edu>
2010-08-27 02:52:35 +0000

    New objects panel: - removed old shapes - removed old receptor/ligand lines - added compartments

--- src/gui/org/pathvisio/gui/swing/MainPanel.java
+++ src/gui/org/pathvisio/gui/swing/MainPanel.java
@@ -204,11 +204,11 @@ public class MainPanel extends JPanel implements VPathwayListener, ApplicationEv
 		objectsPane = new ObjectsPane(swingEngine);
 		int numItemsPerRow = 10;
 		objectsPane.addButtons(actions.newShapeActions, "Basic Shapes", numItemsPerRow);
-		objectsPane.addButtons(actions.newCellularComponentActions, "Cellular components", numItemsPerRow);
 		objectsPane.addButtons(actions.newMIMShapeActions, "MIM shapes", numItemsPerRow);
 		objectsPane.addButtons(actions.newInteractionActions, "Basic interactions", numItemsPerRow);
-		objectsPane.addButtons(actions.newRLInteractionActions, "Receptor/ligand", numItemsPerRow);
+		//objectsPane.addButtons(actions.newRLInteractionActions, "Receptor/ligand", numItemsPerRow);
 		objectsPane.addButtons(actions.newMIMInteractionActions, "MIM interactions", numItemsPerRow);
+		objectsPane.addButtons(actions.newCellularComponentActions, "Cellular compartments", numItemsPerRow);
 		
 		propertyTable = new JTable(model) {
 

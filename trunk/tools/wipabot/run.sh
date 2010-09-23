84264bbb769afe67cb7ef7156cbf36ef58063b3d
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2010-09-23 15:17:08 +0000

    wipabot run script

---
+++ trunk/tools/wipabot/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+CLASSPATH=build:\
+lib/pircbot.jar:\
+../../lib/apache/xmlrpc-common-3.0.jar:\
+../../lib/apache/xmlrpc-client-3.0.jar:\
+../../wikipathways-client.jar 
+
+java -classpath $CLASSPATH org.wikipathways.WipaBotMain

4294ce23a9a425cca0d27b821a75a3af3f3eaa0d
Martina Summer-Kutmon <mkutmon@users.noreply.github.com>
2011-06-20 14:55:14 +0000

    update gex activator

--- modules/org.pathvisio.gex/src/org/pathvisio/gexplugin/Activator.java
+++ modules/org.pathvisio.gex/src/org/pathvisio/gexplugin/Activator.java
@@ -8,15 +8,10 @@ public class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		System.out.println("ACTIVATOR GexPlugin PLUGIN");
 
-//		ServiceReference refPvDesktop = context.getServiceReference(PvDesktop.class.getName());
-//		PvDesktop pvDesktop = (PvDesktop) context.getService(refPvDesktop);
 		GexPlugin plugin = new GexPlugin();
 		context.registerService(Plugin.class.getName(), plugin, null);
-		
-		
-//		plugin.init(pvDesktop);
+
 	}
 
 	@Override

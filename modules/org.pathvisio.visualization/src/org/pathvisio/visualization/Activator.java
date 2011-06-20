f71d76d5d0f26056375357193cea6d92b03a7581
Martina Summer-Kutmon <mkutmon@users.noreply.github.com>
2011-06-20 14:55:39 +0000

    update visualization activator

--- modules/org.pathvisio.visualization/src/org/pathvisio/visualization/Activator.java
+++ modules/org.pathvisio.visualization/src/org/pathvisio/visualization/Activator.java
@@ -9,14 +9,10 @@ public class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		System.out.println("ACTIVATOR VISUALIZATION PLUGIN");
 
-//		ServiceReference refPvDesktop = context.getServiceReference(PvDesktop.class.getName());
-//		PvDesktop pvDesktop = (PvDesktop) context.getService(refPvDesktop);
-		
 		VisualizationPlugin plugin = new VisualizationPlugin();
 		context.registerService(Plugin.class.getName(), plugin, null);
-//		plugin.init(pvDesktop);
+
 	}
 
 	@Override

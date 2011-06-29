3d026ed8ae8127b0b6cd56cfcb5ff0ea9166aeca
martijn <amarillion@users.noreply.github.com>
2011-06-29 17:37:25 +0000

    Small fixes

--- pathvisio.sh
+++ pathvisio.sh
@@ -5,4 +5,5 @@ cd $(dirname $0)
 
 # run PathVisio, try to load plugins in visplugins.jar 
 # this option will be ignored if visplugins.jar does not exist.
-java -jar pathvisio.jar -p visplugins.jar "$@"
+java -cp modules/org.pathvisio.launcher.jar:lib/org.eclipse.osgi.jar:modules/org.pathvisio.core.jar org.pathvisio.launcher.PathVisioMain
+ "$@"

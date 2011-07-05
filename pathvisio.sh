cb5dd768ce17a44fa79e9c0636a22f8353360bbf
martijn <amarillion@users.noreply.github.com>
2011-07-05 13:13:43 +0000

    Fixed small typo in pathvisio.sh

--- pathvisio.sh
+++ pathvisio.sh
@@ -5,5 +5,4 @@ cd $(dirname $0)
 
 # run PathVisio, try to load plugins in visplugins.jar 
 # this option will be ignored if visplugins.jar does not exist.
-java -cp modules/org.pathvisio.launcher.jar:lib/org.eclipse.osgi.jar:modules/org.pathvisio.core.jar org.pathvisio.launcher.PathVisioMain
- "$@"
+java -cp modules/org.pathvisio.launcher.jar:lib/org.eclipse.osgi.jar:modules/org.pathvisio.core.jar org.pathvisio.launcher.PathVisioMain "$@"

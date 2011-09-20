cff41690f270b5ea1a67dcee161fed963b5614ea
martijn <amarillion@users.noreply.github.com>
2011-09-20 19:48:16 +0000

    Make webstart possible using jar-of-jars approach. Temporarily disable -v option.

--- pathvisio.sh
+++ pathvisio.sh
@@ -3,6 +3,5 @@
 # change to directory of this script
 cd $(dirname $0)
 
-# run PathVisio, try to load plugins in visplugins.jar 
-# this option will be ignored if visplugins.jar does not exist.
-java -jar modules/org.pathvisio.launcher.jar "$@"
+# Run PathVisio
+java -jar pathvisio.jar "$@"

bf065a57725102c19c23ad55fe956eae38bd4ba1
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2011-09-21 15:46:27 +0000

    Fix gpmldiff / gpmlpatch

--- trunk/gpmldiff.sh
+++ trunk/gpmldiff.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-java -ea -classpath pathvisio_core.jar org.pathvisio.core.gpmldiff.GpmlDiff "$@"
+CLASSPATH=\
+modules/org.pathvisio.core.jar:\
+lib/com.springsource.org.jdom-1.1.0.jar:\
+lib/org.bridgedb.jar:\
+lib/org.bridgedb.bio.jar
+  
+java -ea -classpath $CLASSPATH org.pathvisio.core.gpmldiff.GpmlDiff "$@"
--- trunk/gpmlpatch.sh
+++ trunk/gpmlpatch.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-java -ea -cp pathvisio.jar org.pathvisio.core.gpmldiff.PatchMain "$@" 
+
+CLASSPATH=\
+modules/org.pathvisio.core.jar:\
+lib/com.springsource.org.jdom-1.1.0.jar:\
+lib/org.bridgedb.jar:\
+lib/org.bridgedb.bio.jar
+
+java -ea -classpath $CLASSPATH org.pathvisio.core.gpmldiff.PatchMain "$@" 

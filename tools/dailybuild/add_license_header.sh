a46c8354486ddc9d8ebcd9cb4043ca7e4c956a26
manager <manager@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2011-09-21 16:32:54 +0000

    Added missing license headers

--- tools/dailybuild/add_license_header.sh
+++ tools/dailybuild/add_license_header.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # extract the license header from a file that has it:
-head -n 16 ../../src/core/org/pathvisio/Engine.java > license.txt
+head -n 16 ../../modules/org.pathvisio.core/src/org/pathvisio/core/Engine.java > license.txt
 # create a list of files missing the license header (exclude Revision.java)
 find ../.. -wholename "**/org/pathvisio/**/*.java" \
 	! -name "Revision.java" \
-	! -wholename "**/src-axis-wpclient/**" \
+	! -wholename "**/org.wikipathways.client/gensrc/**" \
 	| xargs -d '\n' grep -L "Apache License, Version 2.0" \
   > license_check.txt
 # add contents of license.txt at the beginning of those files.

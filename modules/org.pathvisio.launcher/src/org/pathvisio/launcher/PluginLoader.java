436f5ae7e0addcffe9047f3f779d307cad9441ff
Martina Summer-Kutmon <mkutmon@users.noreply.github.com>
2011-10-04 10:05:50 +0000

    added comments and todo

--- modules/org.pathvisio.launcher/src/org/pathvisio/launcher/PluginLoader.java
+++ modules/org.pathvisio.launcher/src/org/pathvisio/launcher/PluginLoader.java
@@ -24,6 +24,15 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 
+/**
+ * when the command line option -p is used
+ * the plugins will be installed and started
+ * 
+ * TODO: currently logging = system.out (find way to log properly)
+ * 
+ * @author martina
+ *
+ */
 public class PluginLoader {
 	
 	private BundleContext context;
@@ -33,8 +42,8 @@ public class PluginLoader {
 	public void installPlugins(List<String> pluginLocations, BundleContext context) {
 		plugins = new ArrayList<Bundle>();
 		this.context = context;
-		for(String s : pluginLocations) {
-			loadFromParameter(s);
+		for(String location : pluginLocations) {
+			loadFromParameter(location);
 		}
 	}
 	
@@ -42,9 +51,9 @@ public class PluginLoader {
 	 * If argument is a dir, load all jar files in that dir recursively
 	 * If argument is a single jar file, load that jar file
 	 */
-	void loadFromParameter(String param) {
+	void loadFromParameter(String filePath) {
 		//See if the plugin is a file
-		File file = new File(param);
+		File file = new File(filePath);
 		
 		if(file.exists()) {
 			// see if it is a directory
@@ -57,27 +66,20 @@ public class PluginLoader {
 				}
 			// see if it is a jar file.
 			} else if(file.getName().endsWith(".jar")) {
-				System.out.println("Detected plugin argument as jar " + param);
+				System.out.println("Detected plugin argument as jar " + filePath);
 				installFromFile(file);
 			}
 		} else {
-			System.out.println("Plug-in was not installed. Could not find plug-in bundle " + param + ".");
+			System.out.println("Plug-in was not installed. Could not find plug-in bundle " + filePath + ".");
 		}
 		
 		for(Bundle bundle : plugins) {
-			boolean success = false;
     		try {
     			bundle.start();
-    			success = true;
     			System.out.println("Bundle " + bundle.getSymbolicName() + " started");
     		} catch (BundleException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-    			if (!success) {
-    				System.out.println("Bundle " + bundle.getSymbolicName() + " failed to start.");
-    			}
-    		}
+    			System.out.println("Bundle " + bundle.getSymbolicName() + " failed to start.");
+			}
 		}
 	}
 	

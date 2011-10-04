cf48011c3478804591dda7bb39e4548d62a2e28a
tina <tina@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2011-10-04 10:01:52 +0000

    fixed -p command line option

--- trunk/modules/org.pathvisio.launcher/src/org/pathvisio/launcher/PluginLoader.java
+++ trunk/modules/org.pathvisio.launcher/src/org/pathvisio/launcher/PluginLoader.java
@@ -17,21 +17,21 @@
 package org.pathvisio.launcher;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.JarFile;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
-import org.pathvisio.core.debug.Logger;
-import org.pathvisio.core.util.FileUtils;
 
 public class PluginLoader {
 	
 	private BundleContext context;
-
+	
+	private List<Bundle> plugins;
+	
 	public void installPlugins(List<String> pluginLocations, BundleContext context) {
+		plugins = new ArrayList<Bundle>();
 		this.context = context;
 		for(String s : pluginLocations) {
 			loadFromParameter(s);
@@ -41,69 +41,56 @@ public class PluginLoader {
 	/**
 	 * If argument is a dir, load all jar files in that dir recursively
 	 * If argument is a single jar file, load that jar file
-	 * If argument is a class name, load that class from current class path
 	 */
 	void loadFromParameter(String param) {
 		//See if the plugin is a file
 		File file = new File(param);
+		
 		if(file.exists()) {
 			// see if it is a directory
 			if(file.isDirectory()) {
-				Logger.log.info ("Looking for plugins in directory " + file.getAbsolutePath());
-				for(File f : FileUtils.getFiles(file, "jar", true)) {
-					loadFromJar (f, param);
+				System.out.println("Looking for plugins in directory " + file.getAbsolutePath());
+				for(File f : file.listFiles()) {
+					if(f.getName().endsWith(".jar")) {
+						installFromFile (f);
+					}
 				}
-				return;
-			}
 			// see if it is a jar file.
-			if(file.getName().endsWith(".jar"))
-			{
-				Logger.log.info("Detected plugin argument as jar " + param);
-				loadFromJar(file, param);
-				return;
+			} else if(file.getName().endsWith(".jar")) {
+				System.out.println("Detected plugin argument as jar " + param);
+				installFromFile(file);
 			}
 		} else {
-			Logger.log.error("Plug-in was not installed. Could not find plug-in bundle " + param + ".");
+			System.out.println("Plug-in was not installed. Could not find plug-in bundle " + param + ".");
+		}
+		
+		for(Bundle bundle : plugins) {
+			boolean success = false;
+    		try {
+    			bundle.start();
+    			success = true;
+    			System.out.println("Bundle " + bundle.getSymbolicName() + " started");
+    		} catch (BundleException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+    			if (!success) {
+    				System.out.println("Bundle " + bundle.getSymbolicName() + " failed to start.");
+    			}
+    		}
 		}
 	}
 	
 	/**
-	 * @param param original parameter that triggered this file to be included,
-	 * this helps in understanding where plugins come from
+	 * installs a bundle from file
 	 */
-	void loadFromJar(File file, String param)
-	{
+	void installFromFile(File file) {
 		try {
-			//Disable security manager, so we can load plugin jars from
-			//the .PathVisio folder, while using webstart
-//			System.setSecurityManager(null);
-			
-			JarFile jarFile = new JarFile(file);
-			Logger.log.trace("\tLoading from jar file " + jarFile);
-			
-			try {
-				Bundle bundle = context.installBundle(file.toURI().toString());
-				boolean success = false;
-	    		try {
-	    			bundle.start();
-	    			success = true;
-	    			Logger.log.info("Bundle " + bundle.getSymbolicName() + " started");
-	    		}
-	    		finally {
-	    			if (!success) {
-	    				System.out.println("Bundle " + bundle.getSymbolicName() + " failed to start.");
-	    			}
-	    		}
-				
-				
-			} catch (BundleException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		catch (IOException ex)
-		{
-			ex.printStackTrace();
+			Bundle bundle = context.installBundle(file.toURI().toString());
+			plugins.add(bundle);				
+		} catch (BundleException e) {
+			System.out.println("Could not install bundle from file " + file.getName());
+			e.printStackTrace();
 		}
 	}
 }

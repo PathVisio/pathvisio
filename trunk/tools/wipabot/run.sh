783f4303d00cbcebd27b8eae9810c4c0af17b215
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2011-10-05 20:56:42 +0000

    Updated wipabot classpath

--- trunk/tools/wipabot/run.sh
+++ trunk/tools/wipabot/run.sh
@@ -4,6 +4,15 @@ CLASSPATH=build:\
 lib/pircbot.jar:\
 ../../lib/apache/xmlrpc-common-3.0.jar:\
 ../../lib/apache/xmlrpc-client-3.0.jar:\
-../../wikipathways-client.jar 
+../../modules/wikipathways-client.jar:\
+../../lib/axis-1_4/lib/axis-ant.jar:\
+../../lib/axis-1_4/lib/axis.jar:\
+../../lib/axis-1_4/lib/commons-discovery-0.2.jar:\
+../../lib/axis-1_4/lib/commons-logging-1.0.4.jar:\
+../../lib/axis-1_4/lib/jaxrpc.jar:\
+../../lib/axis-1_4/lib/log4j-1.2.8.jar:\
+../../lib/axis-1_4/lib/saaj.jar:\
+../../lib/axis-1_4/lib/wsdl4j-1.5.1.jar:\
+../../modules/org.pathvisio.core.jar
 
 java -classpath $CLASSPATH org.wikipathways.WipaBotMain

584242ef665b4623d7b262157874578185336895
martijn <amarillion@users.noreply.github.com>
2011-12-06 15:51:25 +0000

    Fixed bug where anchor doesn't exactly follow mouse on connector. Also simplified code and added unit test.

--- modules/org.pathvisio.core/src/org/pathvisio/core/model/SegmentedConnector.java
+++ modules/org.pathvisio.core/src/org/pathvisio/core/model/SegmentedConnector.java
@@ -26,67 +26,58 @@ import org.pathvisio.core.view.LinAlg.Point;
 
 /**
  * Base class for segmented connectors.
- * @author thomas
- *
  */
-public abstract class SegmentedConnector extends AbstractConnector {
-	protected double locateOnSegment(Segment seg, Point2D v) {
-		return LinAlg.toLineCoordinates(
-				new Point(seg.getMStart()),
-				new Point(seg.getMEnd()),
-				new Point(v));
-	}
+public abstract class SegmentedConnector extends AbstractConnector 
+{
+	protected Point2D fromLineCoordinate(double l, Segment[] segments) 
+	{
+		double totalLength = getTotalLength(segments);
+		
+		double pixelsRemaining = totalLength * l;
+		if (pixelsRemaining < 0) pixelsRemaining = 0;
+		if (pixelsRemaining > totalLength) pixelsRemaining = totalLength;
 
-	protected Point2D fromLineCoordinate(double l, Segment[] segments) {
-		double length = 0;
-		for(Segment s : segments) {
-			length += Math.abs(s.getMLength());
-		}
-
-		//Find the right segment
-		double end = 0;
+		// count off each segment from pixelsRemaining, until there aren't enough pixels left
 		Segment segment = null;
-		int i = 0;
-		for(Segment s : segments) {
-			double slength = Math.abs(s.getMLength());
-			end += slength;
-			double ls = (end - slength) / length;
-			double le = end / length;
-			if(l >= ls && l <= le) {
-				segment = s;
-				break;
+		double slength = 0.0;
+		for(Segment s : segments) 
+		{
+			slength = s.getMLength();
+			segment = s;
+			if (pixelsRemaining < slength) 
+			{
+				break; // not enough pixels left, we found our segment.
 			}
-			i++;
+			pixelsRemaining -= slength;
 		}
-		if(segment == null) segment = segments[segments.length - 1];
 
 		//Find the location on the segment
-		double slength = Math.abs(segment.getMLength());
-		double leftover = (l - (end - slength) / length) * length;
-		double relative = slength == 0 ? 0 : leftover / slength;
-		
 		Point2D s = segment.getMStart();
 		Point2D e = segment.getMEnd();
 
-		double vsx = s.getX();
-		double vsy = s.getY();
-		double vex = e.getX();
-		double vey = e.getY();
+		// protection against division by 0
+		if (slength == 0)
+		{
+			return new Point2D.Double(s.getX(), s.getY());
+		}
+		else
+		{
+			// start from s, in the direction of e, for pixelRemaining pixels.
+			double deltax = e.getX() - s.getX();
+			double deltay = e.getY() - s.getY();
 
-		int dirx = vsx > vex ? -1 : 1;
-		int diry = vsy > vey ? -1 : 1;
-
-		return new Point2D.Double(
-				vsx + dirx * Math.abs(vsx - vex) * relative,
-				vsy + diry * Math.abs(vsy - vey) * relative
-		);
+			return new Point2D.Double(
+					s.getX() + deltax / slength * pixelsRemaining,
+					s.getY() + deltay / slength * pixelsRemaining
+			);
+		}
 	}
 
-	protected Shape calculateShape(Segment[] segments) {
+	protected Shape calculateShape(Segment[] segments) 
+	{
 		GeneralPath path = new GeneralPath();
-		int i = 0;
-		for(Segment s : segments) {
-			i++;
+		for(Segment s : segments) 
+		{
 			if(s == null) { //Ignore null segments
 				Logger.log.error("Null segment in connector!");
 				continue;
@@ -97,12 +88,31 @@ public abstract class SegmentedConnector extends AbstractConnector {
 		return path;
 	}
 
-	protected Segment findSegment(Segment[] segments, Point2D v) {
-		Segment foundSeg = null;
-		double closestFit = Double.MAX_VALUE;
-		double currFit;
-		for (Segment seg:segments) 
-		{
+	/** @returns sum of the lengths of the segments */
+	double getTotalLength (Segment[] segments) 
+	{
+		double totLength = 0.0;
+		for (Segment seg : segments)
+			totLength += seg.getMLength();
+		return totLength;
+	}
+	
+	public Point2D fromLineCoordinate(double l) {
+		return fromLineCoordinate(l, getSegments());
+	}
+	
+	public double toLineCoordinate(Point2D v) 
+	{
+		Segment[] segments = getSegments();
+		double totLength = getTotalLength(segments);
+
+		/* we're looking for the segment that has the shortest (projected) distance to v */
+		double bestDistance = Double.MAX_VALUE; // the shortest distance found thus far
+		double bestInLineCoordinates = 0.0; // the best result so far, in line coordinates. 
+		
+		double runningLength = 0.0; // sum of the length of the segments so far
+		for (Segment seg : segments) 
+		{			
 			// projection of v on the segment 
 			// TODO: probably could be merged with LinAlg.project.
 			// Couldn't do that right away because I need the intermediate u value.
@@ -110,67 +120,39 @@ public abstract class SegmentedConnector extends AbstractConnector {
 			Point direction = new Point(seg.getMEnd()).subtract(new Point(seg.getMStart()));
 			Point vrelative = new Point(v).subtract(new Point(seg.getMStart()));
 
-			double u = ((vrelative.x)*(direction.x) + (vrelative.y) * (direction.y)) 
-			/ ((direction.x) * (direction.x) + (direction.y) * (direction.y));
+			double u = (vrelative.x * direction.x + vrelative.y * direction.y) 
+			/ (direction.x * direction.x + direction.y * direction.y);
 
-			Point projection = new Point(base.x + u * direction.x, base.y + u * (direction.y));
+			Point projection = new Point(base.x + u * direction.x, base.y + u * direction.y);
 
+			// calculate distance between v and segment.
 			// special case: if u is smaller than 0 or larger than 1
-			// then closest lies outside the segment.
-			if (u < 0) currFit = LinAlg.distance(new Point(v), new Point(seg.getMStart()));
-			else if (u > 1) currFit = LinAlg.distance(new Point(v), new Point(seg.getMEnd()));
-			else currFit = LinAlg.distance (projection, new Point(v));
-
-			if (currFit < closestFit) {
-				closestFit = currFit;
+			// then the projection lies outside the segment.
+			double distance; 
+			if (u < 0)
+			{
+				u = 0;
+				distance = LinAlg.distance(new Point(v), new Point(seg.getMStart()));
 			}
-		}
-		return foundSeg;
-	}
-
-	protected double getTotalLength() {
-		double totLength = 0.0;
-		for (Segment seg:getSegments()) {
-			totLength = seg.getMLength() + totLength;
-		}
-		return totLength;
-	}
-	
-	public Point2D fromLineCoordinate(double l) {
-		return fromLineCoordinate(l, getSegments());
-	}
-
-	/* *
-	 *   Find how this segment fits in with the whole elbow and return
-	 *      the cursor position may not always be exactly on the segment so need to make best guess.
-	 */
-	protected double locateOnConnector(Segment segment, Point2D v) {
-		double segPercentOfTot = 0.0;
-		double currSegPercentOfTot;
-		double totLength = getTotalLength();
-		for (Segment seg:getSegments()) {
-			currSegPercentOfTot = seg.getMLength() / totLength;
-			if (seg.equals(segment)) {
-				currSegPercentOfTot = currSegPercentOfTot * locateOnSegment(seg, v);
-				segPercentOfTot = currSegPercentOfTot + segPercentOfTot;
-				break;
+			else if (u > 1)
+			{
+				u = 1;
+				distance = LinAlg.distance(new Point(v), new Point(seg.getMEnd()));
 			}
-			segPercentOfTot = currSegPercentOfTot + segPercentOfTot;
+			else distance = LinAlg.distance (projection, new Point(v));
+
+			// did we find a better match?
+			if (distance < bestDistance)
+			{
+				// save it, and calculate what the result would be in line coordinates.
+				bestDistance = distance;
+				bestInLineCoordinates = (runningLength + u * seg.getMLength()) / totLength;
+			}
+			
+			// calculate the sum of the length of the segments so far
+			runningLength += seg.getMLength(); 
 		}
-		return segPercentOfTot;
-	}
-	
-	public double toLineCoordinate(Point2D v) {
-		Segment[] segments = getSegments();
-		Segment seg = findSegment(segments, v);
-		if (seg == null) {
-			//there is no segment, which means it is the same as a StraightConnectorShape
-			return LinAlg.toLineCoordinates(
-					new Point(segments[0].getMStart()),
-					new Point(segments[segments.length-1].getMEnd()),
-					new Point(v)
-			);
-		}
-		return locateOnConnector(seg, v);
+
+		return bestInLineCoordinates;
 	}
 }

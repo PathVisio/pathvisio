11c5dda7c3fb55bd26708f4517a63d14e792d92f
martijn <amarillion@users.noreply.github.com>
2012-09-12 09:03:40 +0000

    Use an iterator style interface to loop over rows in a DataInterface -> should be quicker

--- modules/org.pathvisio.data/src/org/pathvisio/data/DataInterface.java
+++ modules/org.pathvisio.data/src/org/pathvisio/data/DataInterface.java
@@ -12,9 +12,9 @@ import org.bridgedb.Xref;
 public interface DataInterface
 {
     public ISample getSample(int id) throws DataException;
-	
-    public ISample findSample(String name) throws DataException;
 
+    public ISample findSample(String name) throws DataException;
+    
     public List<String> getSampleNames() throws DataException;
 
     public List<String> getSampleNames(int dataType) throws DataException;
@@ -23,17 +23,26 @@ public interface DataInterface
 
     /**
      * get all datasouces used in this gex.
+     * This is used to filter out unneeded xrefs as early as possible, to speed up identifier mapping.
      */
     public Set<DataSource> getUsedDatasources() throws DataException;
 
+    /**
+    @deprecated use getIterator() instead
+    */
     public IRow getRow(int rowId) throws DataException;
 
+    /**
+    @deprecated use getIterator() instead
+    */
     public int getNrRow() throws DataException;
 
 	public Map<Integer, ? extends ISample> getSamples() throws DataException;
 
 	public Collection<? extends IRow> getData(Set<Xref> destRefs) throws DataException;
 
+	public Iterable<IRow> getIterator() throws DataException;
+	
 	public boolean isConnected();
 
 	public String getDbName();

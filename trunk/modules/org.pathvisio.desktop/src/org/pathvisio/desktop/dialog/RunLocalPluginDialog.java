c7f56359c70ede5393291662a6c55c31fef37ec3
tina <tina@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-01-30 15:54:40 +0000

    fixed small bug in new install local plugins dialog

--- trunk/modules/org.pathvisio.desktop/src/org/pathvisio/desktop/dialog/RunLocalPluginDialog.java
+++ trunk/modules/org.pathvisio.desktop/src/org/pathvisio/desktop/dialog/RunLocalPluginDialog.java
@@ -1,3 +1,19 @@
+// PathVisio,
+// a tool for data visualization and analysis using Biological Pathways
+// Copyright 2006-2013 BiGCaT Bioinformatics
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package org.pathvisio.desktop.dialog;
 
 import java.awt.BorderLayout;
@@ -25,31 +41,40 @@ import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
-public class RunLocalPluginDialog extends JDialog {
+/**
+ * dialog that allows the user to specify a directory
+ * all OSGi bundles in that directory will be installed 
+ * and started
+ * status information on each will be shown in the
+ * StatusDialog dialog
+ * 
+ * @author martina
+ *
+ */
+public class RunLocalPluginDialog extends JDialog 
+{
 
 	private JDialog dlg;
 	private PvDesktop desktop;
 	private JTextField tfDir;
+	private CellConstraints cc = new CellConstraints();
 	
-	public RunLocalPluginDialog(PvDesktop desktop) {
+	public RunLocalPluginDialog(PvDesktop desktop)
+	{
 		super();
 		dlg = this;
 		this.desktop = desktop;
 	}
 	
-	public void createAndShowGUI() {
-		dlg.setPreferredSize(new Dimension(600, 200));
-		dlg.setTitle("Plug-in Manager");
-		dlg.setBackground(Color.white);
-		dlg.setLayout(new BorderLayout());
-		dlg.setResizable(false);
+	public void createAndShowGUI() 
+	{
+		init();
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.white);
 		FormLayout layout = new FormLayout("4dlu, pref, 4dlu, min, 4dlu, min, 4dlu", "4dlu, pref, 4dlu, pref");
 		panel.setLayout(layout);
 		
-		CellConstraints cc = new CellConstraints();
 		panel.add(new JLabel("Run local plugins temporarily. Please select directory."), cc.xy(2, 2));
 		tfDir = new JTextField();
 		JButton browseBtn = new JButton("Browse");
@@ -57,26 +82,38 @@ public class RunLocalPluginDialog extends JDialog {
 		panel.add(tfDir, cc.xy(2, 4));
 		panel.add(browseBtn, cc.xy(4, 4));
 		
+		dlg.add(createBtnPanel(), BorderLayout.SOUTH);
+		dlg.add(panel, BorderLayout.CENTER);
+
+		dlg.setVisible(true);
+	}
+	
+	private JPanel createBtnPanel() 
+	{
 		JPanel btnPanel = new JPanel();
 		btnPanel.setBackground(Color.white);
 		
 		JButton cancelBtn = new JButton("Cancel");
-		cancelBtn.addActionListener(new ActionListener() {
-			
+		cancelBtn.addActionListener(new ActionListener() 
+		{	
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) 
+			{
 				dlg.dispose();
 			}
 		});
 		
 		JButton startBtn = new JButton("Start");
-		startBtn.addActionListener(new ActionListener() {
-			
+		startBtn.addActionListener(new ActionListener() 
+		{	
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(!tfDir.getText().equals("")) {
+			public void actionPerformed(ActionEvent e) 
+			{
+				if(!tfDir.getText().equals("")) 
+				{
 					File file = new File(tfDir.getText());
-					if(file.exists()) {
+					if(file.exists()) 
+					{
 						Map<String, String> map = desktop.getPluginManagerExternal().runLocalPlugin(file);
 						desktop.getPluginManager().startPlugins();
 						dlg.dispose();
@@ -89,51 +126,50 @@ public class RunLocalPluginDialog extends JDialog {
 	
 		btnPanel.add(startBtn);
 		btnPanel.add(cancelBtn);
-		
-		dlg.add(btnPanel, BorderLayout.SOUTH);
-		dlg.add(panel, BorderLayout.CENTER);
-
+		return btnPanel;
+	}
+	
+	private void init() 
+	{
+		dlg.setPreferredSize(new Dimension(600, 200));
+		dlg.setTitle("Install local plugin");
+		dlg.setBackground(Color.white);
+		dlg.setLayout(new BorderLayout());
+		dlg.setResizable(false);
 		dlg.setAlwaysOnTop(true);
 		dlg.setModal(true);
 		dlg.pack();
 		dlg.setLocationRelativeTo(desktop.getFrame());
-		dlg.setVisible(true);
 	}
 	
-	
-	private class StatusDialog extends JDialog {
+	private class StatusDialog extends JDialog 
+	{
 		
 		private JDialog dlg;
 		private PvDesktop desktop;
 		
-		public StatusDialog(PvDesktop desktop) {
+		public StatusDialog(PvDesktop desktop) 
+		{
 			super();
 			dlg = this;
 			this.desktop = desktop;
 		}
 		
-		public void createAndShowGUI(Map<String, String> status) {
+		public void createAndShowGUI(Map<String, String> status) 
+		{
+			init();
 			
+			// splits up bundles that started with out problems
+			// and bundles that couldn't be started
 			Map<String, String> running = new HashMap<String, String>();
 			Map<String, String> problems = new HashMap<String, String>();
 			
-			for(String str : status.keySet()) {
-				if(status.get(str).equals("running")) {
-					running.put(str, status.get(str));
-				} else {
-					problems.put(str, status.get(str));
-				}
+			for(String str : status.keySet()) 
+			{
+				if(status.get(str).equals("running")) running.put(str, status.get(str));
+				else problems.put(str, status.get(str));
 			}
-			
-			dlg.setPreferredSize(new Dimension(600, 400));
-			dlg.setTitle("Run local plugin");
-			dlg.setBackground(Color.white);
-			dlg.setLayout(new BorderLayout());
-			dlg.setResizable(false);
-		
-			dlg.setLayout(new BorderLayout());
-			
-			CellConstraints cc = new CellConstraints();
+
 			String rowLayout = "4dlu,pref,";
 			for(int i = 0; i < running.size(); i++) {
 				rowLayout = rowLayout + "4dlu,pref,";
@@ -146,9 +182,10 @@ public class RunLocalPluginDialog extends JDialog {
 			FormLayout layout = new FormLayout("4dlu, pref, 4dlu, 150dlu, 4dlu", rowLayout);
 			PanelBuilder builder = new PanelBuilder(layout);
 			builder.setBackground(Color.white);
-			builder.addLabel("Bundles started:", cc.xy(2, 2));
-			builder.addSeparator("", cc.xyw(2, 3, 3));
-			
+			if(running.size() > 0) {
+				builder.addLabel("Bundles started:", cc.xy(2, 2));
+				builder.addSeparator("", cc.xyw(2, 3, 3));
+			}
 			int count = 4;
 			for(String b : running.keySet()) {
 				builder.add(new JLabel("    " + b), cc.xy(2, count));
@@ -162,8 +199,10 @@ public class RunLocalPluginDialog extends JDialog {
 				count = count + 2;
 			}
 			count = count+1;
-			builder.addLabel("Problems occured in:", cc.xy(2, count));
-			builder.addSeparator("", cc.xyw(2, count+1, 3));
+			if(problems.size() > 0) {
+				builder.addLabel("Problems occured in:", cc.xy(2, count));
+				builder.addSeparator("", cc.xyw(2, count+1, 3));
+			}
 			count = count+2;
 			
 			for(String b : problems.keySet()) {
@@ -178,30 +217,46 @@ public class RunLocalPluginDialog extends JDialog {
 				count = count + 2;
 			}
 			
+			JScrollPane pane = new JScrollPane(builder.getPanel());
+			pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+			pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+			dlg.add(pane, BorderLayout.CENTER);
+			dlg.add(createBtnPanel(), BorderLayout.SOUTH);
+
+			dlg.setVisible(true);
+		}
+		
+		private JPanel createBtnPanel() 
+		{
 			JPanel btnPanel = new JPanel();
 			btnPanel.setBackground(Color.white);
 			
 			JButton cancelBtn = new JButton("OK");
-			cancelBtn.addActionListener(new ActionListener() {
-				
+			cancelBtn.addActionListener(new ActionListener() 
+			{				
 				@Override
-				public void actionPerformed(ActionEvent e) {
+				public void actionPerformed(ActionEvent e)
+				{
 					dlg.dispose();
 				}
 			});
 			
 			btnPanel.add(cancelBtn);
-			JScrollPane pane = new JScrollPane(builder.getPanel());
-			pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-			pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-			dlg.add(pane, BorderLayout.CENTER);
-			dlg.add(btnPanel, BorderLayout.SOUTH);
-			
+			return btnPanel;
+		}
+		
+		private void init() 
+		{
+			dlg.setLayout(new BorderLayout());
+			dlg.setPreferredSize(new Dimension(600, 450));
+			dlg.setTitle("Status");
+			dlg.setBackground(Color.white);
+			dlg.setLayout(new BorderLayout());
+			dlg.setResizable(false);
 			dlg.setAlwaysOnTop(true);
 			dlg.setModal(true);
 			dlg.pack();
 			dlg.setLocationRelativeTo(desktop.getFrame());
-			dlg.setVisible(true);
 		}
 	}
 }

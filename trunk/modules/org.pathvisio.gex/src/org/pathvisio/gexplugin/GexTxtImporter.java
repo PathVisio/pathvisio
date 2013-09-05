a0244f0df7723efeded63a09a35fb4312f63adb7
tina <tina@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-09-05 13:28:26 +0000

    bug fix 1659 - data import has more information now, error messages should be more informative and specific now.

--- trunk/modules/org.pathvisio.gex/src/org/pathvisio/gexplugin/GexTxtImporter.java
+++ trunk/modules/org.pathvisio.gex/src/org/pathvisio/gexplugin/GexTxtImporter.java
@@ -18,9 +18,11 @@ package org.pathvisio.gexplugin;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.sql.SQLException;
 import java.sql.Types;
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -79,238 +81,267 @@ public class GexTxtImporter
 		}
 
 		StopWatch timer = new StopWatch();
-		try
-		{
-			if (p != null) p.report("\nCreating expression dataset");
+			if (p != null) p.report("\nReading data set ...");
 
-			//Create a new expression database (or overwrite existing)
-			result = new SimpleGex(info.getGexName(), true, gexManager.getDBConnector());
+			//Create a new data database (or overwrite existing)
+			try {
+				result = new SimpleGex(info.getGexName(), true, gexManager.getDBConnector());
+			} catch (Exception e) {
+				if (p != null) p.report("Error: Could not import the dataset. Invalid data file.\n");
+				Logger.log.error("Error: Could not import the dataset. Invalid data file.", e);
+				return;
+			} 
 
-			if (p != null)
-			{
-				p.report("Importing data");
-				p.report("> Processing headers");
-			}
 			timer.start();
-
-			BufferedReader in = new BufferedReader(new FileReader(info.getTxtFile()));
+			
 			//Get the number of lines in the file (for progress)
 			int nrLines = FileUtils.getNrLines(info.getTxtFile().toString());
-
-			String[] headers = info.getColNames();
-			//Parse sample names and add to Sample table
-			result.prepare();
-			int sampleId = 0;
-			List<Integer> dataCols = new ArrayList<Integer>();
-			for(int i = 0; i < headers.length; i++)
-			{
-				if(p != null && p.isCancelled())
-				{
-					//User pressed cancel
-					result.close();
-					error.close();
-					return;
-				}
-
-				ColumnType type = info.getColumnType(i);
-				//skip the id and systemcode column if there is one
-				if(type == ColumnType.COL_NUMBER ||	type == ColumnType.COL_STRING)
-				{
-					String header = headers[i];
-					if (header.length() >= 50)
-					{
-						header = header.substring(0, 49);
-					}
-					try {
-						result.addSample(
-							sampleId++,
-							header,
-							type == ColumnType.COL_STRING ? Types.CHAR : Types.REAL);
-						dataCols.add(i);
-					}
-					catch(Error e) {
-						errors = reportError(info, error, "Error in headerline, can't add column " + i +
-							" due to: " + e.getMessage(), errors);
-
-					}
-				}
-			}
-
-			if (p != null) p.report("> Processing lines");
-
-			//Check ids and add expression data
-			for(int i = 0; i < info.getFirstDataRow(); i++) in.readLine(); //Go to line where data starts
-			String line = null;
-			int n = info.getFirstDataRow();
-			int added = 0;
-			int worked = importWork / nrLines;
-
+			
 			boolean maximumNotSet = true;
 			boolean minimumNotSet = true;
 			double maximum = 1; // Dummy value
 			double minimum = 1; // Dummy value
+			int added = 0;
 
-			NumberFormat nf = NumberFormat.getInstance(
-					info.digitIsDot() ? Locale.US : Locale.FRANCE);
-			
-			info.dataRowsImported = 0;
-			info.rowsMapped = 0;
-			
-			while((line = in.readLine()) != null)
-			{
-				if(p != null && p.isCancelled())
-				{
-					result.close();
-					error.close();
-					return;
-				} //User pressed cancel
-				String[] data = line.split(info.getDelimiter(), headers.length);
-				n++;
-				if(n == info.headerRow) continue; //Don't add header row (very unlikely that this will happen)
-				if(data.length < headers.length) {
-					errors = reportError(info, error, "Number of columns in line " + n +
-							"doesn't match number of header columns",
-							errors);
-					continue;
-				}
-				info.dataRowsImported++;
+			try {
+				BufferedReader in = new BufferedReader(new FileReader(info.getTxtFile()));
 				
-				if (p != null) p.setTaskName("Importing expression data - processing line " + n + "; " + errors + " exceptions");
-				//Check id and add data
-				String id = data[info.getIdColumn()].trim();
-
-				/*Set the system code to the one found in the dataset if there is a system code column,
-				 * otherwise set the system code to the one selected (either by the user or by regular
-				 * expressions.*/
-				DataSource ds;
-				if (info.isSyscodeFixed())
+				String[] headers = info.getColNames();
+				//Parse sample names and add to Sample table
+				result.prepare();
+				int sampleId = 0;
+				List<Integer> dataCols = new ArrayList<Integer>();
+				
+				for(int i = 0; i < headers.length; i++)
 				{
-					ds = info.getDataSource();
-				}
-				else
-				{
-					ds = DataSource.getBySystemCode(data[info.getSyscodeColumn()].trim());
-				}
-				Xref ref = new Xref (id, ds);
-
-				//check if the ref exists
-				boolean refExists = currentGdb.xrefExists(ref);
-
-				if(!refExists)
-				{
-					errors = reportError(info, error, "Line " + n + ":\t" + ref +
-							"\tError: Could not look up this identifier in the synonym database", errors);
-				} else {
-					errors = reportError(info, error, "Line " + n + ":\t" + ref +
-							"\t", errors - 1); // decrement counter to count only the errors
-					info.rowsMapped++;
-				}
-				// add row anyway
-				{
-					boolean success = true;
-					for(int col : dataCols)
+					if(p != null && p.isCancelled())
 					{
-						String value = data[col];
+						//User pressed cancel
+						result.close();
+						error.close();
+						in.close();
+						return;
+					}
 
-						if(!info.isStringCol(col)
-								&& (value == null || value.equals(""))) {
-							value = "NaN";
-						}
-
-						//Determine maximum and minimum values.
-
-						try
+					ColumnType type = info.getColumnType(i);
+					//skip the id and systemcode column if there is one
+					if(type == ColumnType.COL_NUMBER ||	type == ColumnType.COL_STRING)
+					{
+						String header = headers[i];
+						if (header.length() >= 50)
 						{
-							double dNumber = nf.parse(value.toUpperCase()).doubleValue();
-							value = "" + dNumber;
-							if(maximumNotSet || dNumber>maximum)
-							{
-								maximum=dNumber;
-								maximumNotSet=false;
-							}
-
-							if(minimumNotSet || dNumber<minimum)
-							{
-								minimum=dNumber;
-								minimumNotSet=false;
-							}
+							header = header.substring(0, 49);
 						}
-						catch (ParseException e)
-						{
-							// we've got a number in a non-number column.
-							// safe to ignore
-							Logger.log.warn ("Number format exception in non-string column " + e.getMessage());
+						try {
+							result.addSample(
+								sampleId++,
+								header,
+								type == ColumnType.COL_STRING ? Types.CHAR : Types.REAL);
+							dataCols.add(i);
 						}
+						catch(Error e) {
+							errors = reportError(info, error, "Error in headerline, can't add column " + i +
+								" due to: " + e.getMessage(), errors);
 
-						//End of determining maximum and minimum values. After the data has been read,
-						//maximum and minimum will have their correct values.
-
-						try
-						{
-							result.addExpr(
-									ref,
-									Integer.toString(dataCols.indexOf(col)),
-									value,
-									added);
-						}
-						catch (Exception e)
-						{
-							errors = reportError(info, error, "Line " + n + ":\t" + line + "\n" +
-									"\tException: " + e.getMessage(), errors);
-							success = false;
 						}
 					}
-					if(success) added++;
 				}
-				if (p != null) p.worked(worked);
-			}
-			in.close();
+				
+				if (p != null) p.report("Processing " + nrLines + " lines ...");
+				
+				//Check ids and add expression data
+				for(int i = 0; i < info.getFirstDataRow(); i++) in.readLine(); //Go to line where data starts
+				
+				String line = null;
+				int n = info.getFirstDataRow();
+				
+				int worked = importWork / nrLines;
+				
+				NumberFormat nf = NumberFormat.getInstance(
+						info.digitIsDot() ? Locale.US : Locale.FRANCE);
+				
+				info.dataRowsImported = 0;
+				info.rowsMapped = 0;
+				
+				while((line = in.readLine()) != null)
+				{
+					if(p != null && p.isCancelled())
+					{
+						result.close();
+						error.close();
+						in.close();
+						return;
+					} //User pressed cancel
+					String[] data = line.split(info.getDelimiter(), headers.length);
+					n++;
+					if(n == info.headerRow) continue; //Don't add header row (very unlikely that this will happen)
+					if(data.length < headers.length) {
+						errors = reportError(info, error, "Number of columns in line " + n +
+								"doesn't match number of header columns",
+								errors);
+						continue;
+					}
+					info.dataRowsImported++;
+					
+					if (p != null) p.setTaskName("Importing expression data - processing line " + n + "; " + errors + " exceptions");
+					//Check id and add data
+					String id = data[info.getIdColumn()].trim();
 
-			//Data is read and written to the database
+					/*Set the system code to the one found in the dataset if there is a system code column,
+					 * otherwise set the system code to the one selected (either by the user or by regular
+					 * expressions.*/
+					DataSource ds;
+					if (info.isSyscodeFixed())
+					{
+						ds = info.getDataSource();
+					}
+					else
+					{
+						ds = DataSource.getBySystemCode(data[info.getSyscodeColumn()].trim());
+					}
+					Xref ref = new Xref (id, ds);
+					
+					//check if the ref exists
+					boolean refExists = currentGdb.xrefExists(ref);
+					if(!refExists)
+					{
+						errors = reportError(info, error, "Line " + n + ":\t" + ref +
+								"\tError: Could not look up this identifier in the synonym database", errors);
+					} else {
+						errors = reportError(info, error, "Line " + n + ":\t" + ref +
+								"\t", errors - 1); // decrement counter to count only the errors
+						info.rowsMapped++;
+					}		
+					
+					// add row anyway
+					{
+						boolean success = true;
+						for(int col : dataCols)
+						{
+							String value = data[col];
 
+							if(!info.isStringCol(col)
+									&& (value == null || value.equals(""))) {
+								value = "NaN";
+							}
+
+							//Determine maximum and minimum values.
+
+							try
+							{
+								double dNumber = nf.parse(value.toUpperCase()).doubleValue();
+								value = "" + dNumber;
+								if(maximumNotSet || dNumber>maximum)
+								{
+									maximum=dNumber;
+									maximumNotSet=false;
+								}
+
+								if(minimumNotSet || dNumber<minimum)
+								{
+									minimum=dNumber;
+									minimumNotSet=false;
+								}
+							}
+							catch (ParseException e)
+							{
+								// we've got a number in a non-number column.
+								// safe to ignore
+								Logger.log.warn ("Number format exception in non-string column " + e.getMessage());
+							}
+
+							//End of determining maximum and minimum values. After the data has been read,
+							//maximum and minimum will have their correct values.
+
+							try
+							{
+								result.addExpr(
+										ref,
+										Integer.toString(dataCols.indexOf(col)),
+										value,
+										added);
+							}
+							catch (Exception e)
+							{
+								errors = reportError(info, error, "Line " + n + ":\t" + line + "\n" +
+										"\tException: " + e.getMessage(), errors);
+								success = false;
+							}
+						}
+						//Data is read and written to the database
+						if(success) added++;
+					}
+					if (p != null) p.worked(worked);
+				}
+				in.close();
+			} catch (FileNotFoundException e) {
+				if (p != null) p.report("Error: Could not read data file. Check if file exists and you have write permissions in the specific directories.\n");
+				Logger.log.error("Error: Could not read data file.", e);
+				return;
+			} catch (IOException e) {
+				if (p != null) p.report("Error: Could not read data file. Check if file exists and you have write permissions in the specific directories.\n");
+				Logger.log.error("Error: Could not read data file.", e);
+				return;
+			} catch (IDMapperException e) {
+				if (p != null) p.report("Error: ID mapping database connection does not work. Make sure you have a valid BridgeDB database.\n");
+				Logger.log.error("Error: ID mapping database connection does not work. Make sure you have a valid BridgeDB database.\n", e);
+				return;
+			} catch (DataException e) {
+				if (p != null) p.report("Error: Could not load data file.\n Please restart PathVisio and retry the data import.");
+				Logger.log.error("Error: Could not load data file.\n Please restart PathVisio and retry the data import.", e);
+				return;
+			} catch (SQLException e) {
+				if (p != null) p.report("Error: Could not load data file.\n Please restart PathVisio and retry the data import.");
+				Logger.log.error("Error: Could not load data file.\n Please restart PathVisio and retry the data import.", e);
+				return;
+			} 
+			
 			//Writing maximum and minimum to ImportInformation
 			info.setMaximum(maximum);
 			info.setMinimum(minimum);
 
-			if (p != null) p.report(added + " rows of data were imported succesfully");
-			if(errors > 0)
-			{
-				if (p != null) p.report(errors + " exceptions occured, see file '" + errorFile + "' for details");
-			} else {
-				new File(errorFile).delete(); // If no errors were found, delete the error file
-			}
-			if (p != null)
-			{
-				p.setTaskName("Finalizing database (this may take some time)");
-				p.report ("Finalizing database");
-			}
+			try {
+				// writing out result of data import
+				if (p != null)
+				{
+					p.report("\n" + added + " rows of data were imported succesfully.");
+					Thread.sleep(1000);
+					if(errors > 0)
+					{
+						p.report(errors + " identifiers were not recognized, for more information check the log file:\n\t" + errorFile);
+					} else {
+						p.report("All identifiers in the dataset were recognized.");
+						new File(errorFile).delete(); // If no errors were found, delete the error file
+					}
+					Thread.sleep(1000);
+					p.setTaskName("Finalizing database (this may take some time)");
+					p.report ("\nFinalizing database...");
+				}
 
-			result.finalize();
-			if (p != null) p.worked(finalizeWork);
+			
+				result.finalize();
+				if (p != null) p.worked(finalizeWork);
+				
+				error.println("Time to create expression dataset: " + timer.stop());
+				error.close();
 
-			error.println("Time to create expression dataset: " + timer.stop());
-			error.close();
-
-			gexManager.setCurrentGex(result.getDbName(), false);
-			if (p != null)
-			{
-				p.setTaskName ("Done.");
-				p.report ("Done.");
-				p.finished();
+				gexManager.setCurrentGex(result.getDbName(), false);
+				if (p != null)
+				{
+					p.setTaskName ("Done.");
+					p.report ("\nDone.");
+					p.finished();
+				}
+			} catch(IDMapperException e) {
+				if (p != null) p.report("Error: database could not be finalized!\nPlease verify that you have enough memory and write permissions.\n");
+				Logger.log.error("Expression data import error", e);
+				return;
+			} catch (InterruptedException e) {
+			} catch (DataException e) {
+				if (p != null) p.report("Error: Could not set dataset as current dataset in PathVisio.\n Please restart PathVisio and retry the data import.");
+				Logger.log.error("Error: Could not set dataset as current dataset in PathVisio.\n Please restart PathVisio and retry the data import.", e);
+				return;
 			}
-		}
-		catch(Exception e)
-		{
-			if (p != null) p.report("Import aborted due to error: " + e.getMessage());
-			Logger.log.error("Expression data import error", e);
-			try
-			{
-				result.close();
-			}
-			catch (DataException f)
-			{ Logger.log.error ("Exception while aborting database", f); }
-			error.close();
-		}
 	}
 
 	private static int reportError(ImportInformation info, PrintStream log, String message, int nrError)

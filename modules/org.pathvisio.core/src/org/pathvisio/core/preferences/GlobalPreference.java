f096dcd18a589dd093f0e4ddf0ae1ea233111b5b
Martina Summer-Kutmon <mkutmon@users.noreply.github.com>
2015-02-27 16:20:53 +0000

    make default gray for data not found lighter

--- modules/org.pathvisio.core/src/org/pathvisio/core/preferences/GlobalPreference.java
+++ modules/org.pathvisio.core/src/org/pathvisio/core/preferences/GlobalPreference.java
@@ -38,7 +38,7 @@ public enum GlobalPreference implements Preference
 
 	COLOR_NO_CRIT_MET(new Color(180, 220, 180)),
 	COLOR_NO_GENE_FOUND(new Color(180, 220, 220)),
-	COLOR_NO_DATA_FOUND(new Color(180, 180, 180)),
+	COLOR_NO_DATA_FOUND(new Color(216, 216, 216)),
 	COLOR_SELECTED(Color.RED),
 	COLOR_HIGHLIGHTED(Color.GREEN),
 	COLOR_LINK(Color.BLUE),

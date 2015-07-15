1b4270914633638f79164173ba9897baa1f8d999
Egon Willighagen 0000-0001-7542-0286 <egonw@users.noreply.github.com>
2015-07-15 18:23:26 +0000

    Fixed a null pointer exception cause

--- modules/org.pathvisio.core/src/org/pathvisio/core/biopax/BiopaxElement.java
+++ modules/org.pathvisio.core/src/org/pathvisio/core/biopax/BiopaxElement.java
@@ -180,7 +180,9 @@ public class BiopaxElement extends PathwayElement
 					//by pubmed id
 					BiopaxProperty bp1 = e.getProperty(PropertyType.ID.name());
 					BiopaxProperty bp2 = elm.getProperty(PropertyType.ID.name());
-					if(!bp1.getValue().equals("") && !bp2.getValue().equals("") && bp1.getValue().equals(bp2.getValue())) {
+					if(bp1 != null && !"".equals(bp1.getValue()) &&
+					   bp2 != null && !"".equals(bp2.getValue()) &&
+					    bp1.getValue().equals(bp2.getValue())) {
 						Logger.log.trace("Equal pubmed id!");
 						elm.setId(e.getId());
 						return;

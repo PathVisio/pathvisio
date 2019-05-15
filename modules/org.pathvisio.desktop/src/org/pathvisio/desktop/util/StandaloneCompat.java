58055689f084cb3ac89659616fe4cb71c58f694b
Martina Summer-Kutmon <mkutmon@users.noreply.github.com>
2019-05-15 15:37:10 +0200

    update license header in desktop module

--- modules/org.pathvisio.desktop/src/org/pathvisio/desktop/util/StandaloneCompat.java
+++ modules/org.pathvisio.desktop/src/org/pathvisio/desktop/util/StandaloneCompat.java
@@ -1,19 +1,19 @@
-// PathVisio,
-// a tool for data visualization and analysis using Biological Pathways
-// Copyright 2006-2011 BiGCaT Bioinformatics
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*******************************************************************************
+ * PathVisio, a tool for data visualization and analysis using biological pathways
+ * Copyright 2019 BiGCaT Bioinformatics
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package org.pathvisio.desktop.util;
 
 import javax.swing.JOptionPane;
@@ -25,14 +25,13 @@ import org.pathvisio.desktop.PvDesktop;
 /**
  * Collection of compatibility "hacks" for the standalone PathVisio app.
  */
-public class StandaloneCompat implements GdbEventListener//, GexManagerListener
+public class StandaloneCompat implements GdbEventListener
 {
 	private final PvDesktop desktop;
 
 	public StandaloneCompat (PvDesktop desktop)
 	{
 		this.desktop = desktop;
-//		desktop.getGexManager().addListener(this);
 		desktop.getSwingEngine().getGdbManager().addGdbEventListener(this);
 	}
 
@@ -56,26 +55,4 @@ public class StandaloneCompat implements GdbEventListener//, GexManagerListener
 			break;
 		}
 	}
-
-//	public void gexManagerEvent(GexManagerEvent e)
-//	{
-//		if (e.getType() == GexManagerEvent.CONNECTION_OPENED)
-//		{
-//			try
-//			{
-//				if (desktop.getGexManager().getCurrentGex().getUsedDatasources().contains(BioDataSource.ENSEMBL))
-//				{
-//					JOptionPane.showMessageDialog(desktop.getFrame(),
-//							"The selected data set contains old references to Ensembl\n" +
-//							"It is recommended that you redo the dataset import process");
-//				}
-//			}
-//			catch (DataException ex)
-//			{
-//				//ignore - no compatibility check today
-//			}
-//		}
-//
-//	}
-
 }
--- modules/org.pathvisio.desktop/test/org/pathvisio/desktop/debug/TestAndMeasure.java
+++ modules/org.pathvisio.desktop/test/org/pathvisio/desktop/debug/TestAndMeasure.java
@@ -1,22 +1,20 @@
-// PathVisio,
-// a tool for data visualization and analysis using Biological Pathways
-// Copyright 2006-2011 BiGCaT Bioinformatics
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*******************************************************************************
+ * PathVisio, a tool for data visualization and analysis using biological pathways
+ * Copyright 2019 BiGCaT Bioinformatics
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package org.pathvisio.desktop.debug;
-
-import buildsystem.Measure;
 
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
@@ -25,8 +23,6 @@ import java.io.File;
 
 import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
-
-import junit.framework.TestCase;
 
 import org.bridgedb.Xref;
 import org.bridgedb.bio.BioDataSource;
@@ -42,6 +38,9 @@ import org.pathvisio.core.view.Line;
 import org.pathvisio.core.view.VPathway;
 import org.pathvisio.core.view.VPathwayElement;
 import org.pathvisio.gui.view.VPathwaySwing;
+
+import buildsystem.Measure;
+import junit.framework.TestCase;
 
 /**
  * Test memory usage and speed of 

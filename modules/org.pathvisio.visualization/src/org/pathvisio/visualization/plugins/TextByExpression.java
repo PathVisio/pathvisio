778ce3ca7db134649294ef6ebd4fab6fdd8958fe
Martina Summer-Kutmon <mkutmon@users.noreply.github.com>
2024-10-03 13:38:05 +0200

    updating license header text

--- modules/org.pathvisio.visualization/src/org/pathvisio/visualization/plugins/TextByExpression.java
+++ modules/org.pathvisio.visualization/src/org/pathvisio/visualization/plugins/TextByExpression.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * PathVisio, a tool for data visualization and analysis using biological pathways
- * Copyright 2006-2019 BiGCaT Bioinformatics
+ * Copyright 2006-2024 PathVisio
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
